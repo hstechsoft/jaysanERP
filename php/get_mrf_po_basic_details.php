8627f1b480f5e1ea0c03ccb05d184d7e652653b8
nklharish1 <nklharish1@gmail.com>
2025-10-03 11:24:22 +0530

    php po basic details updated

---
+++ php/get_mrf_po_basic_details.php
@@ -0,0 +1,64 @@
+<?php
+ include 'db_head.php';
+
+ 
+
+ 
+ 
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+$data = "'".$data."'";
+return $data;
+}
+
+$sql = "SET time_zone = '+05:30';";
+$sql .= "SELECT
+ sup.creditors_terms as terms,
+    sup.creditor_name as sub_name,
+    sup.creditors_addr as sub_addr,
+    sup.creditor_gst as sub_gst,
+    sup.state_name as sub_state_name,
+    sup.contact_person as sub_contact_person,
+    sup.contact as sub_contact,
+    sup.creditors_email as sub_email,
+     con.creditor_name as con_name,
+   con.creditors_addr ascon_addr,
+   con.creditor_gst ascon_gst,
+   con.state_name ascon_state_name,
+   con.contact_person ascon_contact_person,
+   con.contact ascon_contact,
+   con.creditors_email ascon_email,
+   (SELECT company.company_address from company LIMIT 1) as company_address,
+     REPLACE((SELECT company.company_address from company LIMIT 1) , '\n', '<br>') as company_address
+    
+FROM
+    `mrf_purchase`
+    LEFT JOIN creditors sup on mrf_purchase.po_order_to = sup.creditor_id
+    LEFT JOIN creditors con on mrf_purchase.po_order_to = con.creditor_id";
+
+if ($conn->multi_query($sql)) {
+    do {
+        if ($result = $conn->store_result()) {
+            if ($result->num_rows > 0) {
+                $rows = array();
+                while ($r = $result->fetch_assoc()) {
+                    $rows[] = $r;
+                }
+                echo json_encode($rows);
+            } else {
+                echo "0 result";
+            }
+            $result->free();
+        }
+    } while ($conn->more_results() && $conn->next_result());
+} else {
+    echo "Error: " . $conn->error;
+}
+$conn->close();
+
+
+ ?>
+
+
