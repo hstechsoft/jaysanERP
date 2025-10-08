9a265f85a038c6e4471965148c9e298e4e308583
nklharish1 <nklharish1@gmail.com>
2025-10-08 11:57:53 +0530

    po_material sts added

--- php/insert_purchase_order.php
+++ php/insert_purchase_order.php
@@ -49,9 +49,9 @@ if ($conn->multi_query($sql)) {
       $disc = $po_materials['disc'];
        $due_on = $po_materials['due_on'];
  
-      
+      echo  $po_material_id;
       $batch_id = sql_nullable( $batch_id );
-      $po_material_id = sql_nullable( $po_material_id );
+ 
 
 echo  $batch_id;
   
