4eb350de85bb7f7c7c96f1e3c253b77e7d67bba3
nklharish1 <nklharish1@gmail.com>
2025-10-11 10:56:19 +0530

    md approval changed

--- php/update_rate_quotation.php
+++ php/update_rate_quotation.php
@@ -2,8 +2,8 @@
 include 'db_head.php';
 
 $rqid =($_POST['rqid']);
-$part_spec_data_json = $conn->real_escape_string($part_spec_data);
-$part_spec_data_json = json_encode($part_spec_data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+$part_spec_data_json = $conn->real_escape_string("part_spec_data");
+$part_spec_data_json = json_encode("part_spec_data", JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 
 
 $sql_delete = "DELETE from rate_quotation_spec WHERE rqid = '$rqid '" ;
@@ -13,7 +13,7 @@ $sql_delete = "DELETE from rate_quotation_spec WHERE rqid = '$rqid '" ;
 
 if ($conn->query($sql_delete) === TRUE) {
  $insert_part_spec = "INSERT  INTO rate_quotation_spec (rqid,spec_details)
-VALUES ($last_id,'$part_spec_data')";
+VALUES ($rqid,'$part_spec_data')";
  
  if ($conn->query($insert_part_spec) === TRUE) {
 echo "ok";
