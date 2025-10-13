3272ebd6ba769e0d01131dda3f1acacb16e162d7
nklharish1 <nklharish1@gmail.com>
2025-10-13 18:54:49 +0530

    po dashboard edited

---
+++ php/get_material_frequency_chart.php
@@ -0,0 +1,41 @@
+<?php
+ include 'db_head.php';
+
+ 
+$part_id = test_input(($_GET['part_id']));
+
+ 
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+$data = "'".$data."'";
+return $data;
+}
+
+$sql = "SET time_zone = '+05:30';";
+$sql .= "SELECT sum(req_qty) as total_req_qty,date_only(dated) as for_date from material_request_form mrf WHERE part_id = $part_id  GROUP by dated";
+
+if ($conn->multi_query($sql)) {
+    do {
+        if ($result = $conn->store_result()) {
+            if ($result->num_rows > 0) {
+                $rows = array();
+                while ($r = $result->fetch_assoc()) {
+                    $rows[] = $r;
+                }
+                echo json_encode($rows);
+            } else {
+                echo "0 result";
+            }
+            $result->free();
+        }
+    } while ($conn->more_results() && $conn->next_result());
+} else {
+    echo "Error: " . $conn->error;
+}
+$conn->close();
+
+ ?>
+
+
