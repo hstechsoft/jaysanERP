a2e32ce926aba028e7db8e49f6d40771eba3d7a2
nklharish1 <nklharish1@gmail.com>
2025-10-15 10:17:30 +0530

    emo auto iserted

---
+++ php/get_emp_auto.php
@@ -0,0 +1,40 @@
+<?php
+ include 'db_head.php';
+
+
+ $emp_name = ($_GET['emp_name']);
+
+
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+$data = "'".$data."'";
+return $data;
+}
+
+
+$emp_name  = "%" .  $emp_name ."%";
+
+
+
+
+$sql = "SELECT emp_id,emp_name FROM   employee  WHERE emp_name LIKE  '$emp_name'";
+
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $rows = array();
+    while($r = mysqli_fetch_assoc($result)) {
+        $rows[] = $r;
+    }
+    print json_encode($rows);
+} else {
+  echo "0 result";
+}
+$conn->close();
+
+ ?>
+
+
