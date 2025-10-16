faf9627c9a879e019d7e8b995cb7d8c7ec28e558
nklharish1 <nklharish1@gmail.com>
2025-10-16 18:26:08 +0530

    po_report

--- php/get_po_list.php
+++ php/get_po_list.php
@@ -15,25 +15,21 @@ return $data;
 $sql = "SET time_zone = '+05:30';";
 $sql .= "SELECT
     po.*,
-  (SELECT creditors.creditor_name from creditors WHERE creditors.creditor_id = po.po_order_to) as order_to_name,
+    (SELECT creditors.creditor_name 
+     FROM creditors 
+     WHERE creditors.creditor_id = po.po_order_to) AS order_to_name,
+    CASE 
+        WHEN MIN(pom.is_approved) = 1 THEN 1   -- all approved
+        ELSE 0                                 -- at least one not approved
+    END AS all_approved,
     0 AS approve_sts
 FROM
-    `jaysan_po` po
-INNER JOIN jaysan_po_material pom ON
-    po.po_id = pom.jaysan_po_id
-WHERE
-    pom.is_approved = 0 GROUP by po.po_id
-UNION ALL
-SELECT
-    po.*,
-   (SELECT creditors.creditor_name from creditors WHERE creditors.creditor_id = po.po_order_to) as order_to_name,
-    1 AS approve_sts
-FROM
-    `jaysan_po` po
-INNER JOIN jaysan_po_material pom ON
-    po.po_id = pom.jaysan_po_id
-WHERE
-    pom.is_approved = 1 GROUP by po.po_id";
+    jaysan_po po
+INNER JOIN jaysan_po_material pom 
+    ON po.po_id = pom.jaysan_po_id
+GROUP BY
+    po.po_id;
+";
     // jmat.po_material_id = '' AND jp.po_order_to = 1";
 
 if ($conn->multi_query($sql)) {
