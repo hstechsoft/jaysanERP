3fdfe5417ea8b014298b23ac8590a0945c9d9c41
nklharish1 <nklharish1@gmail.com>
2025-11-18 15:10:01 +0530

    role auto complete

---
+++ php/get_appmenu_auto.php
@@ -0,0 +1,40 @@
+<?php
+ include 'db_head.php';
+
+
+ $role = ($_GET['role']);
+
+
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+$data = "'".$data."'";
+return $data;
+}
+
+
+$role  = "%" .  $role ."%";
+
+
+
+
+$sql = "SELECT role FROM   app_menu  WHERE role LIKE  '$role' group by role";
+
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $rows = array();
+    while($r = mysqli_fetch_assoc($result)) {
+        $rows[] = $r;
+    }
+    print json_encode($rows);
+} else {
+  echo "0 result";
+}
+$conn->close();
+
+ ?>
+
+
