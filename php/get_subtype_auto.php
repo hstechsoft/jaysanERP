4e6860f6105ce99a8ac9baf64fad6f1a651e620b
nklharish1 <nklharish1@gmail.com>
2025-11-19 16:08:10 +0530

    sub type

---
+++ php/get_subtype_auto.php
@@ -0,0 +1,38 @@
+<?php
+ include 'db_head.php';
+
+
+ $subtype_name = test_input($_GET['subtype_name']);
+  $mtid = test_input($_GET['mtid']);
+ 
+
+
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+$data = "'".$data."'";
+return $data;
+}
+
+$subtype_name = "%".$subtype_name."%";
+
+$sql = "SELECT * FROM jaysan_model_subtype WHERE subtype_name LIKE $subtype_name and mtid = $mtid " ;
+
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $rows = array();
+    while($r = mysqli_fetch_assoc($result)) {
+        $rows[] = $r;
+    }
+    print json_encode($rows);
+} else {
+  echo "0 result";
+}
+$conn->close();
+
+ ?>
+
+
