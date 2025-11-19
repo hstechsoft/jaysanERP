888b522c1cb4c42302fade234a0db5b0adb14986
nklharish1 <nklharish1@gmail.com>
2025-11-19 23:48:39 +0530

    query

--- php/get_dcf_report.php
+++ php/get_dcf_report.php
@@ -1,98 +1,66 @@
-<?php
- include 'db_head.php';
+with ass_info as(SELECT
+  
+  
+    IFNULL(ap.qty, 0) AS qty,
+    ap.dated,
+    ap.assign_type,
+
+    ap.godown,
+   
+    sop.opid,
+    godown.godown_name,
+    IFNULL(
+        SUM(qty) OVER(
+    PARTITION BY sop.opid
+    ),
+    0
+    ) AS assigned_qty, 
+   sop.required_qty,
+    sop.required_qty -(SUM(IFNULL(qty, 0)) OVER(PARTITION BY sop.opid)) AS unassigned_qty,
+   SUM(ifnull(qty,0)) over (PARTITION by ap.opid order by ap.assign_type) as assigntype_total_count,
 
 
- $customer_query = ($_GET['customer']) == '' ? 1 : "cus.cus_id  = " . ($_GET['customer']);
- $dcrf_sts_query = ($_GET['dcf_sts']) == '' ? 1 : "dcf1.sts  = '" . ($_GET['dcf_sts']) . "'";
-$sof_query = ($_GET['order_no']) == '' ? 1 : "sof.order_no  = " . ($_GET['order_no']);
-
+ sum(IFNULL(qty, 0)) over ( PARTITION BY ap.opid,ap.assign_type,ap.godown ) as finished_godown_count,
+                  sum(IFNULL(qty, 0)) over ( PARTITION BY ap.opid,ap.assign_type,ap.dated ) as production_date_count
  
-function test_input($data) {
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-$data = "'".$data."'";
-return $data;
-}
+                 
+                 
+      
 
-$sql = <<<SQL
-WITH
-    dcf_opid AS(
-    SELECT DISTINCT
-        opid,
-        dcf.dcf_id
-    FROM
-        assign_product ap
-    INNER JOIN dcf ON ap.dcf_id = dcf.dcf_id
-    WHERE
-        1
-),
-sop_details AS(
-    SELECT
-        dcf_opid.*,
-        oid
-    FROM
-        dcf_opid
-    INNER JOIN sales_order_product sop ON
-        dcf_opid.opid = sop.opid
-    ORDER BY
-        dcf_id
-),
-sof_details AS(
-    SELECT
-        sop_details.*,
-        sof.customer_id,
-        sof.order_no,
-        sof.emp_id,
-    sp.product
-    
-    FROM
-        sop_details
-    INNER JOIN sales_order_form sof ON
-        sof.oid = sop_details.oid
-       INNER JOIN sales_product sp ON
-        sp.opid = sop_details.opid where $sof_query
-),
-final AS(
-    SELECT
-        sd.*,
-        cus.cus_name,
-        emp.emp_name
-    FROM
-        sof_details sd
-    INNER JOIN customer cus ON
-        cus.cus_id = sd.customer_id
-    INNER JOIN employee emp ON
-        emp.emp_id = sd.emp_id  where $customer_query
-),
-dcf_final as (SELECT
-    dcf_id,    JSON_ARRAYAGG(
-        JSON_OBJECT('order_no',order_no,'cus_name',cus_name,'product',product,'emp',emp_name)) as sale_order
+   
 FROM
-    final GROUP by dcf_id)
-    
-    SELECT dcf_final.sale_order, dcf1.dcf_id,dcf1.dated,dcf1.consignee,dcf1.sts,DATE_FORMAT(dcf1.dated, '%d-%m-%Y') as dated,(SELECT employee.emp_name from employee WHERE employee.emp_id = dcf1.dcf_by) as emp_name  from dcf_final inner join dcf dcf1 on dcf_final.dcf_id = dcf1.dcf_id WHERE  $dcrf_sts_query ORDER by dcf1.dcf_id DESC
-SQL;
+    assign_product ap
+
+LEFT JOIN godown ON ap.godown = godown.gid
+RIGHT JOIN sales_order_product sop ON
+    ap.opid = sop.opid 
+),
+
+assign_product_details as(select opid,assign_type,qty,godown,
+dated as production_date,godown_name,assigned_qty,required_qty,unassigned_qty,assigntype_total_count,finished_godown_count,production_date_count from ass_info WHERE 1 
+-- AND assign_type in ("Waiting") and unassigned_qty > 1 and godown = 0 and dated BETWEEN '2025-05-10' and '2025-12-10' and  unassigned_qty > 0 
+
+ GROUP by opid,assign_type,production_date,godown),
+dcf_info as(SELECT ap.ass_id,ap.opid,ap.qty,ap.dcf_id ,dcf.sts as dcf_sts,
+sum(qty) over (PARTITION by opid,dcf_id) as dcf_count,
+sum(qty) over (PARTITION by opid) as total_dcf_count
+FROM assign_product ap INNER join dcf on ap.dcf_id = dcf.dcf_id ),
+
+dcf_details as (SELECT opid,dcf_id,dcf_sts,dcf_count,total_dcf_count from dcf_info
+-- WHERE opid = 687 and dcf_sts = 'HOD'
+GROUP by opid,dcf_id order by opid )
+,
+ap_opid as(SELECT opid,assign_type, assigntype_total_count,JSON_ARRAYAGG(
+        JSON_OBJECT('godown_name',godown_name,'production_date' ,production_date , 'finished_godown_count', finished_godown_count, 'production_date_count',production_date_count)) as assign_details,required_qty,assigned_qty,unassigned_qty  from assign_product_details  GROUP by opid,assign_type),
+        
+assign_final as( SELECT opid,required_qty,assigned_qty,unassigned_qty,JSON_ARRAYAGG(
+        JSON_OBJECT('assign_type',assign_type,'assigntype_total_count',assigntype_total_count,'assign_details',assign_details)) as assign_info from ap_opid GROUP by opid)
+        ,
+dcf_final as (SELECT opid, JSON_ARRAYAGG(
+        JSON_OBJECT('dcf_id',dcf_id,'dc_sts',dcf_sts,'dcf_count',dcf_count)) as dcf_details,total_dcf_count from dcf_details GROUP by opid )
+        
+        
+        SELECT * from  sales_order_info_view
 
 
-
-//  $sql = "SELECT  dcf1.dcf_id,dcf1.dated,dcf1.consignee,dcf1.sts,DATE_FORMAT(dcf1.dated, '%d-%m-%Y') as dated,employee.emp_name FROM dcf dcf1 INNER join employee on dcf1.dcf_by = employee.emp_id WHERE 1 ORDER by dcf_by ";
-
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    $rows = array();
-    while($r = mysqli_fetch_assoc($result)) {
-        $rows[] = $r;
-    }
-    print json_encode($rows);
-} else {
-  echo "0 result";
-}
-$conn->close();
-
- ?>
-
-
-
-
+        
--- php/hari_query.sql
+++ php/hari_query.sql
@@ -18,15 +18,10 @@ with ass_info as(SELECT
    sop.required_qty,
     sop.required_qty -(SUM(IFNULL(qty, 0)) OVER(PARTITION BY sop.opid)) AS unassigned_qty,
    SUM(ifnull(qty,0)) over (PARTITION by ap.opid order by ap.assign_type) as assigntype_total_count,
-    SUM(
-    CASE  WHEN assign_type = 'Finshed' THEN IFNULL(ap.qty, 0) ELSE 0 END
-) OVER ( PARTITION BY ap.opid,ap.godown ) AS finished_godown_count,
 
-SUM(
-    CASE  WHEN assign_type = 'Production'   THEN IFNULL(qty, 0) ELSE 0 END
-) OVER ( PARTITION BY ap.opid,ap.assign_type,ap.dated ) AS production_date_count,
- sum(IFNULL(qty, 0)) over ( PARTITION BY ap.opid,ap.assign_type,ap.godown ) as fw,
-                  sum(IFNULL(qty, 0)) over ( PARTITION BY ap.opid,ap.assign_type,ap.dated ) as fw1
+
+ sum(IFNULL(qty, 0)) over ( PARTITION BY ap.opid,ap.assign_type,ap.godown ) as finished_godown_count,
+                  sum(IFNULL(qty, 0)) over ( PARTITION BY ap.opid,ap.assign_type,ap.dated ) as production_date_count
  
                  
                  
@@ -39,11 +34,20 @@ FROM
 LEFT JOIN godown ON ap.godown = godown.gid
 RIGHT JOIN sales_order_product sop ON
     ap.opid = sop.opid 
-)
+),
 
-select opid,assign_type,qty,godown,
-dated as production_date,godown_name,assigned_qty,required_qty,unassigned_qty,assigntype_total_count,finished_godown_count,production_date_count,fw,fw1 from ass_info WHERE 1 
+assign_product_details as(select opid,assign_type,qty,godown,
+dated as production_date,godown_name,assigned_qty,required_qty,unassigned_qty,assigntype_total_count,finished_godown_count,production_date_count from ass_info WHERE 1 
 -- AND assign_type in ("Waiting") and unassigned_qty > 1 and godown = 0 and dated BETWEEN '2025-05-10' and '2025-12-10' and  unassigned_qty > 0 
 
-and opid = 195 GROUP by assign_type,production_date,godown
+and opid = 195 GROUP by assign_type,production_date,godown),
+dcf_info as(SELECT ap.ass_id,ap.opid,ap.qty,ap.dcf_id ,dcf.sts as dcf_sts,
+sum(qty) over (PARTITION by opid,dcf_id) as dcf_count,
+sum(qty) over (PARTITION by opid) as total_dcf_count
+FROM assign_product ap INNER join dcf on ap.dcf_id = dcf.dcf_id ),
 
+dcf_details as (SELECT opid,dcf_id,dcf_sts,dcf_count from dcf_info
+-- WHERE opid = 687 and dcf_sts = 'HOD'
+GROUP by opid,dcf_id order by opid )
+
+SELECT * from assign_product_details
