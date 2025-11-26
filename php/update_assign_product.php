f0015bfc645a62185fb1a8bd95a1015bff21cd67
nklharish1 <nklharish1@gmail.com>
2025-11-26 18:26:00 +0530

    update assign product added

---
+++ php/update_assign_product.php
@@ -0,0 +1,43 @@
+<?php
+ include 'db_head.php';
+
+ $dated = test_input($_GET['dated']);
+$assign_type = test_input($_GET['assign_type']);
+$godown = test_input($_GET['godown']);
+$ass_id = test_input($_GET['ass_id']);
+
+
+ 
+ 
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+$data = "'".$data."'";
+return $data;
+}
+if ($assign_type == "'Production'") {
+  $godown = "'0'";
+
+} elseif ($assign_type == "'Finshed'") {
+    $dated = "'0000-00-00'";
+  // Finished type - keep dated, set godown to 0 is not needed
+} else {
+  echo "Error: Invalid assign_type. Only 'Production' or 'Finished' allowed.";
+  $conn->close();
+  exit();
+}
+
+ $sql =  "UPDATE  assign_product SET dated =  $dated,assign_type =  $assign_type,godown =  $godown WHERE ass_id =  $ass_id";
+
+  if ($conn->query($sql) === TRUE) {
+   echo "ok";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+$conn->close();
+
+ ?>
+
+
+
