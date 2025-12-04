e17be33cf1ce4afc823feac47b433abd696cffc2
nklharish1 <nklharish1@gmail.com>
2025-12-04 16:18:54 +0530

    graph api updated

--- php/get_process_summary.php
+++ php/get_process_summary.php
@@ -20,7 +20,7 @@ WITH RECURSIVE process_details AS (
     UNION ALL 
     SELECT pwl.*,level+1 as level from process_wel_tbl pwl  join process_details on  pwl.process_id = process_details.previous_process_id WHERE 1),
     
-extra as (SELECT wtm.*, COUNT(wtm.ori_process_id) over (PARTITION by wtm.wtid) as  a from process_details  pd LEFT join work_time_master wtm on pd.process_id = wtm.ori_process_id)
+extra as (SELECT pd.process_id as pid, wtm.*, COUNT(wtm.ori_process_id) over (PARTITION by wtm.wtid) as  a from process_details  pd LEFT join work_time_master wtm on pd.process_id = wtm.ori_process_id)
 SELECT 
 JSON_ARRAYAGG(
         JSON_OBJECT(	'creditor_name',
@@ -51,7 +51,8 @@ JSON_ARRAYAGG(
                    ifnull(max_time,0) as max_time,
                   
                    ifnull(ex.cost,0) as cost,
-                    sum(a) as process_extra_count
+                    sum(a) as process_extra_count,
+                    (select jaysan_proces.process_name from jaysan_proces where jaysan_proces.process_id = ex.pid) as process_name
                    from extra ex
     LEFT join creditors cre on ex.godown_id = cre.creditor_id
     LEFT join  department dep on ex.dep_id = dep.dep_id
