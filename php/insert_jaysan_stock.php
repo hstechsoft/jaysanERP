810aedcf38fea75d21648f559c65e3545fe0fcd2
nklharish1 <nklharish1@gmail.com>
2025-12-05 19:16:54 +0530

    jaysan stock added

--- php/insert_jaysan_stock.php
+++ php/insert_jaysan_stock.php
@@ -13,10 +13,6 @@ $finished_godown = ($_GET['finished_godown']);
 sql_nullable($godown);
 sql_nullable($dep);
 sql_nullable($sec);
-sql_nullable($finished_process_no);
-sql_nullable($batch_id);
-sql_nullable($qty);
-
 sql_nullable($finished_godown);
  
  echo $godown;
@@ -40,7 +36,7 @@ return $data;
 
 
 
- $sql = "INSERT INTO jaysan_stock ( godown,dep,sec,finished_process_no,batch_id,qty,finished_godown) VALUES ($godown,$dep,$sec,$finished_process_no,$batch_id,$qty,$finished_godown)";
+ $sql = "INSERT INTO jaysan_stock ( godown,dep,sec,finished_process_no,batch_id,qty,finished_godown) VALUES ('$godown','$dep','$sec','$finished_process_no','$batch_id','$qty','$finished_godown')";
 
   if ($conn->query($sql) === TRUE) {
    echo "ok";
