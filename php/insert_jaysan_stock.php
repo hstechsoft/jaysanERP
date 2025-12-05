0cd4121f833859c1c82674d74130161d1834fa0c
nklharish1 <nklharish1@gmail.com>
2025-12-05 19:21:04 +0530

    jaysan stock added

--- php/insert_jaysan_stock.php
+++ php/insert_jaysan_stock.php
@@ -1,18 +1,19 @@
 <?php
  include 'db_head.php';
 
- $godown = ($_GET['godown']);
-$dep = ($_GET['dep']);
-$sec = ($_GET['sec']);
-$finished_process_no = ($_GET['finished_process_no']);
-$batch_id = ($_GET['batch_id']);
-$qty = ($_GET['qty']);
+ $godown = test_input($_GET['godown']);
+$dep = test_input($_GET['dep']);
+$sec = test_input($_GET['sec']);
+$finished_process_no = test_input($_GET['finished_process_no']);
+$batch_id = test_input($_GET['batch_id']);
+$qty = test_input($_GET['qty']);
 
 $finished_godown = ($_GET['finished_godown']);
 
 sql_nullable($godown);
 sql_nullable($dep);
 sql_nullable($sec);
+
 sql_nullable($finished_godown);
  
  echo $godown;
@@ -36,7 +37,7 @@ return $data;
 
 
 
- $sql = "INSERT INTO jaysan_stock ( godown,dep,sec,finished_process_no,batch_id,qty,finished_godown) VALUES ('$godown','$dep','$sec','$finished_process_no','$batch_id','$qty','$finished_godown')";
+ $sql = "INSERT INTO jaysan_stock ( godown,dep,sec,finished_process_no,batch_id,qty,finished_godown) VALUES ($godown,$dep,$sec,$finished_process_no,$batch_id,$qty,$finished_godown)";
 
   if ($conn->query($sql) === TRUE) {
    echo "ok";
