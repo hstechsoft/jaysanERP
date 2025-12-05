354d05adcfad5f0935091c822b07ccb90c25aa46
nklharish1 <nklharish1@gmail.com>
2025-12-05 19:10:07 +0530

    jaysan stock added

--- php/insert_jaysan_stock.php
+++ php/insert_jaysan_stock.php
@@ -10,7 +10,14 @@ $qty = test_input($_GET['qty']);
 $dated = test_input($_GET['dated']);
 $finished_godown = test_input($_GET['finished_godown']);
 
-
+sql_nullable($godown);
+sql_nullable($dep);
+sql_nullable($sec);
+sql_nullable($finished_process_no);
+sql_nullable($batch_id);
+sql_nullable($qty);
+sql_nullable($dated);
+sql_nullable($finished_godown);
  
  
 function test_input($data) {
