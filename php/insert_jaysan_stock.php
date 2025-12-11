38a0d00695b0b50fc412bb94832c2e5528ea840a
nklharish1 <nklharish1@gmail.com>
2025-12-11 17:43:07 +0530

    query updated

--- php/insert_jaysan_stock.php
+++ php/insert_jaysan_stock.php
@@ -16,6 +16,9 @@ $dep = sql_nullable($dep);
 $sec = sql_nullable($sec);
 
 $finished_godown = sql_nullable($finished_godown);
+$min_qty =  isset($_GET['min_qty']) ? test_input($_GET['min_qty']) : "''";
+$max_qty = isset($_GET['max_qty']) ? test_input($_GET['max_qty']) : "''";
+
 
 
 
@@ -28,7 +31,6 @@ return $data;
 }
 
 
-echo "godown: ".$godown." dep: ".$dep." sec: ".$sec." part_id: ".$part_id." batch_id: ".$batch_id." qty: ".$qty." finished_godown: ".$finished_godown." remark: ".$remark."\n";
 
 // Set MySQL timezone
 $conn->query("SET time_zone = '+05:30'");
@@ -48,7 +50,24 @@ if ($result->num_rows > 0) {
 }
 
   if ($conn->query($sql) === TRUE) {
-   echo "ok";
+$check_master = "SELECT master_id FROM sec_stock_master WHERE godown=$godown AND dep=$dep AND sec=$sec AND part_id=$part_id";
+$master_result = $conn->query($check_master);
+if ($master_result->num_rows > 0) {
+  // Record exists, update it
+  $master_sql = "UPDATE sec_stock_master SET min_qty=$min_qty,max_qty=$max_qty
+      WHERE godown=$godown AND dep=$dep AND sec=$sec AND part_id=$part_id";
+      $conn->query($master_sql);
+} else {
+  // Record doesn't exist, insert it
+  $master_sql = "INSERT INTO sec_stock_master (godown,dep,sec,part_id,min_qty,max_qty) 
+      VALUES ($godown,$dep,$sec,$part_id,$min_qty,$qty)";
+      $conn->query($master_sql);
+}
+    echo "ok";
+
+
+
+
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
   }
