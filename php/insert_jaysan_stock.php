e46b0bf1880234cfc15a17307db73c453e056b29
nklharish1 <nklharish1@gmail.com>
2025-12-11 18:09:58 +0530

    query updated

--- php/insert_jaysan_stock.php
+++ php/insert_jaysan_stock.php
@@ -58,12 +58,23 @@ if ($master_result->num_rows > 0) {
   // Record exists, update it
   $master_sql = "UPDATE sec_stock_master SET min_qty=$min_qty,max_qty=$max_qty
       WHERE godown=$godown AND dep=$dep AND sec=$sec AND part_id=$part_id";
-      $conn->query($master_sql);
+     $master_sql_result =   $conn->query($master_sql);
+      if ($master_sql_result->num_rows > 0) {
+      }
+      else {
+        echo "Error: " . $master_sql . "<br>" . $conn->error;
+      }
+      
 } else {
   // Record doesn't exist, insert it
   $master_sql = "INSERT INTO sec_stock_master (godown,dep,sec,part_id,min_qty,max_qty) 
       VALUES ($godown,$dep,$sec,$part_id,$min_qty,$max_qty)";
-      $conn->query($master_sql);
+      $master_sql_result =   $conn->query($master_sql);
+      if ($master_sql_result->num_rows > 0) {
+      }
+      else {
+        echo "Error: " . $master_sql . "<br>" . $conn->error;
+      }
 }
     echo "ok";
 
