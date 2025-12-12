7b6939e54980746ff8651974433178d11a1ead28
nklharish1 <nklharish1@gmail.com>
2025-12-12 13:36:46 +0530

    auto updated

--- php/get_creditors_auto1.php
+++ php/get_creditors_auto1.php
@@ -2,6 +2,7 @@
  include 'db_head.php';
 
  $term = ($_GET['term']);
+ $part_id  = test_input($_GET['part_id']);
 
 
  
@@ -15,7 +16,7 @@ return $data;
 }
 $term = "%".$term."%";
 
- $sql = "SELECT creditors.*,sec_stock_master.min_qty,sec_stock_master.max_qty FROM creditors left join  sec_stock_master on  creditors.creditor_id =  sec_stock_master.store_id and sec_stock_master.store_type = 'godown' WHERE creditor_name   like  '$term'";
+ $sql = "SELECT creditors.*,sec_stock_master.min_qty,sec_stock_master.max_qty FROM creditors left join  sec_stock_master on  creditors.creditor_id =  sec_stock_master.store_id and sec_stock_master.store_type = 'godown' and sec_stock_master.part_id = $part_id WHERE creditor_name   like  '$term' group by creditors.creditor_id";
 
 $result = $conn->query($sql);
 
