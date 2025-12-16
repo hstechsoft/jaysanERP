f71fede1e69c40be8a1c8003551c4404a0976989
nklharish1 <nklharish1@gmail.com>
2025-12-16 15:48:43 +0530

    insert grn

--- php/insert_purchase_order.php
+++ php/insert_purchase_order.php
@@ -72,6 +72,91 @@ if ($conn->multi_query($sql)) {
 
           if ($conn->query($sql_grn) === TRUE) {
 
+
+$store_id = $details['store_id'];
+    $store_type = $details['store_type'];
+    $dep = '';
+    $godown = '';
+    $sec = '';
+
+    if($store_type == 'godown')
+    {
+     $dep =   '';
+     $sec =  '';
+     $godown =  $store_id;
+    }
+    else if($store_type == 'dep')
+    {
+
+     $sec =  '';
+     
+$sql_store_type = "SELECT * FROM department WHERE dep_id  = $store_id";
+$result = $conn->query($sql_store_type);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $dep =   $row['dep_id'];
+    $godown = $row['godown_id'];
+   
+  }
+}
+
+    }
+      else if($store_type == 'sec')
+    {
+
+    
+     
+$sql_store_type = "SELECT sec.dep_sec_id,dep.dep_id,dep.godown_id FROM `dep_section` sec inner join department dep on sec.dep_id = dep.dep_id WHERE sec.dep_sec_id = $store_id";
+$result = $conn->query($sql_store_type);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $dep =   $row['dep_id'];
+    $godown = $row['godown_id'];
+    $sec = $row['dep_sec_id'];  
+   
+  }
+}
+
+    }
+   $godown = sql_nullable($godown);
+   $dep = sql_nullable($dep);
+   $sec = sql_nullable($sec);
+
+       $check_sql = "SELECT qty FROM jaysan_stock WHERE (godown <=> $godown )AND (dep <=> $dep )AND (sec <=> $sec )AND (part_id = $po_material_id)";
+
+$result = $conn->query($check_sql);
+
+if ($result->num_rows > 0) {
+  // Record exists, update it
+  $qty = $qty + $result->fetch_assoc()['qty'];
+  $remark = "inward stock updated dc". $dc_no1;
+  $sql_stock = "UPDATE jaysan_stock   SET  qty= $qty,remark= '$remark' ,dated = NOW()
+      WHERE (godown <=> $godown )AND (dep <=> $dep )AND (sec <=> $sec )AND (part_id =   $po_material_id)";
+
+       if ($conn->query($sql_stock) === TRUE) {
+   
+  } else {
+    echo "Error: " . $sql_stock . "<br>" . $conn->error;
+  }
+    
+} else {
+  // Record doesn't exist, insert it
+  $remark = "inward stock inserted dc" . $dc_no1;
+  $sql_stock = "INSERT INTO jaysan_stock (godown,dep,sec,part_id,qty,remark) 
+      VALUES ($godown,$dep,$sec, (select po_material_id from jaysan_po_material where jaysan_po_material_id = $jaysan_po_material_id),$qty,'$remark')";
+
+        if ($conn->query($sql_stock) === TRUE) {
+        }
+  else {
+    echo "Error: " . $sql_stock . "<br>" . $conn->error;
+  }
+}
+
+
           } else {
               echo "Error: " . $sql_grn . "<br>" . $conn->error;
           }
