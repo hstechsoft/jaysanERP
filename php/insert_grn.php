0d5b4b692dd156ec1499a5a9b304fad51617dbc4
nklharish1 <nklharish1@gmail.com>
2025-12-16 15:12:43 +0530

    insert grn

--- php/insert_grn.php
+++ php/insert_grn.php
@@ -100,7 +100,7 @@ if ($result->num_rows > 0) {
 } else {
   // Record doesn't exist, insert it
   $remark = "inward stock inserted dc";
-  $sql_stock = "INSERT INTO jaysan_stock (godown,dep,sec,part_id,batch_id,qty,finished_godown,remark) 
+  $sql_stock = "INSERT INTO jaysan_stock (godown,dep,sec,part_id,qty,remark) 
       VALUES ($godown,$dep,$sec, (select po_material_id from jaysan_po_material where jaysan_po_material_id = $jaysan_po_material_id),$qty,'$remark')";
 echo $sql_stock;
         if ($conn->query($sql_stock) === TRUE) {
