43ec8027999f242b0a8ff2780001e81de29c34d5
nklharish1 <nklharish1@gmail.com>
2026-01-22 14:21:03 +0530

    sales order advance updated1

--- php/insert_sales_order_form.php
+++ php/insert_sales_order_form.php
@@ -122,19 +122,18 @@ if($paymentDetails != 0)
      
    
 $amount = test_input($payment['amount']);
-echo 'advance'. $payment['advance_id'].'<br>';
+
 $advance_ref_id = sql_nullable($payment['advance_id']);
-echo 'advance1-'. $advance_ref_id.'<br>';
+
 $payment_id_advance = "NULL";
 if($advance_ref_id == "NULL")
 $payment_id_advance = $payment_id;
 
-echo "payment_id_advance: ".$payment_id_advance;
 
 
   
       $sql_insert_advance = "INSERT INTO sale_payment_advance (payment_id,amount,oid,cus_id,advance_ref_id) VALUES ($payment_id_advance,$amount,$oid,$customer_id,$advance_ref_id)";
-echo $sql_insert_advance;
+
       if ($conn->query($sql_insert_advance) === TRUE) {
           
       } else {
