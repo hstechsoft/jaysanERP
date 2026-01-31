7aac47d300b8c8a20a73f44addb8a261c573d2ae
nklharish1 <nklharish1@gmail.com>
2026-01-31 17:31:29 +0530

    advance corrected

--- php/insert_sales_order_form.php
+++ php/insert_sales_order_form.php
@@ -90,6 +90,8 @@ if($payment['advance_id']>0)
 if($payment['advance_id']>0)
 {
         echo "Advance already available do not pay from advance.";
+          http_response_code(422);
+
       exit();
 }
     }
