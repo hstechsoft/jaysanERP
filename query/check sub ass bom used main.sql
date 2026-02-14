fe6fd6b55669b44edd043931a401d7f283754791
nklharish1 <nklharish1@gmail.com>
2026-02-14 09:23:46 +0530

    duplicate bom finder added

---
+++ query/check sub ass bom used main.sql
@@ -0,0 +1,8 @@
+-- Active: 1766425908618@@srv1002.hstgr.io@3306@u333142350_jaysan
+SELECT bom_output.bom_id FROM `bom_output`
+inner join bom_input on bom_output.bom_id = bom_input.bom_id
+INNER join parts_tbl outpart on bom_output.part_id = outpart.part_id
+inner join  parts_tbl inpart on bom_input.part_id = inpart.part_id
+
+WHERE outpart.sub_ass = 0 and inpart.sub_ass = 1 group by bom_output.bom_id
+
