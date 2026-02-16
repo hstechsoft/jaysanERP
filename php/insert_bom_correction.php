fbb997ee90a39e96359f1910a43312683969da61
nklharish1 <nklharish1@gmail.com>
2026-02-16 17:56:08 +0530

    bom correction added

--- php/insert_bom_correction.php
+++ php/insert_bom_correction.php
@@ -4,6 +4,7 @@
  $outpart_bom_id = test_input($_POST['outpart_bom_id']);
 $bomlist_id = test_input($_POST['bomlist_id']);
 $part_id = test_input($_POST['part_id']);
+$bom_output_id = test_input($_POST['bom_output_id']);
 
 
  
@@ -17,7 +18,7 @@ return $data;
 }
 
 
-$sql = "INSERT INTO bom_correction (outpart_bom_id, bomlist_id, part_id) VALUES ($outpart_bom_id, $bomlist_id, $part_id) ON DUPLICATE KEY UPDATE bomlist_id = $bomlist_id";
+$sql = "INSERT INTO bom_correction (outpart_bom_id, bomlist_id, part_id, bom_output_id) VALUES ($outpart_bom_id, $bomlist_id, $part_id, $bom_output_id) ON DUPLICATE KEY UPDATE bomlist_id = $bomlist_id";
 
 if ($conn->query($sql) === TRUE) {
   echo "ok";
