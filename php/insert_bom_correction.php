b10e4b8456c8860c45ae24e5ccc1a7800da76950
nklharish1 <nklharish1@gmail.com>
2026-02-16 12:52:12 +0530

    bom correction added

---
+++ php/insert_bom_correction.php
@@ -0,0 +1,31 @@
+<?php
+ include 'db_head.php';
+
+ $outpart_bom_id = test_input($_POST['outpart_bom_id']);
+$bomlist_id = test_input($_POST['bomlist_id']);
+$part_id = test_input($_POST['part_id']);
+
+
+ 
+ 
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+$data = "'".$data."'";
+return $data;
+}
+
+
+ $sql = "INSERT INTO bom_correction ( outpart_bom_id,bomlist_id,part_id) VALUES ($outpart_bom_id,$bomlist_id,$part_id)";
+
+  if ($conn->query($sql) === TRUE) {
+   echo "ok";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+$conn->close();
+
+ ?>
+
+
