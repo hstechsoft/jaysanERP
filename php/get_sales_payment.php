642dfb37ccff67a0f23cb4921490bf69f82218aa
nklharish1 <nklharish1@gmail.com>
2026-02-28 14:55:29 +0530

    dcf payment corrected

--- php/get_sales_payment.php
+++ php/get_sales_payment.php
@@ -15,7 +15,7 @@ return $data;
 }
 
 
- $sql = "SELECT  concat('₹',FORMAT(jaysan_payment.amount, 0)) as amount,DATE_FORMAT(jaysan_payment.payment_date, '%d-%m-%Y %h:%i %p') as date_f ,jaysan_payment.sts,(SELECT sales_order_form.order_no from sales_order_form WHERE sales_order_form.oid = jaysan_payment.oid) as order_no from jaysan_payment WHERE jaysan_payment.oid in (SELECT sales_order_form.oid from sales_order_form WHERE sales_order_form.order_no in ( $oid_arr )) order by order_no";
+ $sql = "SELECT  concat('₹',FORMAT(jaysan_payment.amount, 0)) as amount,DATE_FORMAT(jaysan_payment.payment_date, '%d-%m-%Y %h:%i %p') as date_f ,jaysan_payment.sts,(SELECT sales_order_form.order_no from sales_order_form WHERE sales_order_form.oid = jaysan_payment.oid) as order_no from jaysan_payment WHERE jaysan_payment.sts = 'approved' andjaysan_payment.oid in (SELECT sales_order_form.oid from sales_order_form WHERE sales_order_form.order_no in ( $oid_arr )) order by order_no";
 
 $result = $conn->query($sql);
 
