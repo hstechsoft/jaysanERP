dd767b156c7fd6b075e86a42bb850bbbd0235956
nklharish1 <nklharish1@gmail.com>
2026-02-28 11:13:42 +0530

    sales price

--- php/get_jaysan_model_subtype_sale.php
+++ php/get_jaysan_model_subtype_sale.php
@@ -16,11 +16,72 @@ return $data;
 }
 
 
-$sql = "SELECT
+// $sql = "SELECT
+//     subtype_group_id,
+//     subgroup_type_price.min_price as w,
+//     IFNULL(group_type_price.max_price, 0) AS max_price,
+//     IFNULL(group_type_price.min_price, 0) AS min_price,
+//     IFNULL(group_type_price.mrp, 0) AS mrp,
+//     (
+//     SELECT
+//         dep_section.sec_name
+//     FROM
+//         dep_section
+//     WHERE
+//         dep_section.dep_sec_id = subtype_group_id
+// ) AS sec_name,
+// JSON_ARRAYAGG(
+//     JSON_OBJECT(
+//         'msid',
+//         jaysan_model_subtype.msid,
+//         'subtype_name',
+//         subtype_name,
+//         'is_default',
+//         is_default,
+//         'is_reduce',
+//         is_reduce,
+//         'max_price',
+//         IFNULL(group_type_price.max_price, 0),
+//         'min_price',
+//         IFNULL(group_type_price.min_price, 0),
+//         'mrp',
+//         IFNULL(group_type_price.mrp, 0),
+//         'price',
+//         IFNULL(subgroup_subtype_price.price, 0),
+//         'discount',
+//         IFNULL(
+//             subgroup_subtype_price.discount,
+//             0
+//         )
+//     )
+// ) AS price_details
+// FROM
+//     jaysan_model_subtype
+// LEFT JOIN group_type_price ON jaysan_model_subtype.mtid = group_type_price.mtid AND group_type_price.group_id =(
+//     SELECT
+//         group_id
+//     FROM
+//         customer_subgroup_master
+//     WHERE
+//         sub_group_id = $subgroup_id
+// )
+// LEFT JOIN subgroup_subtype_price ON jaysan_model_subtype.msid = subgroup_subtype_price.msid AND subgroup_subtype_price.sub_group_id = $subgroup_id
+// WHERE
+//     jaysan_model_subtype.mtid = $mtid
+// GROUP BY
+//     subtype_group_id
+// ORDER BY
+//     subtype_group_id
+// DESC";
+
+
+$sql = <<<SQL
+SELECT
     subtype_group_id,
-    IFNULL(group_type_price.max_price, 0) AS max_price,
-    IFNULL(group_type_price.min_price, 0) AS min_price,
-    IFNULL(group_type_price.mrp, 0) AS mrp,
+ 
+    IFNULL(subgroup_type_price.max_price, 0) AS max_price,
+    IFNULL(subgroup_type_price.min_price, 0) AS min_price,
+    IFNULL(subgroup_type_price.mrp, 0) AS mrp,
     (
     SELECT
         dep_section.sec_name
@@ -40,11 +101,11 @@ JSON_ARRAYAGG(
         'is_reduce',
         is_reduce,
         'max_price',
-        IFNULL(group_type_price.max_price, 0),
+        IFNULL(subgroup_type_price.max_price, 0),
         'min_price',
-        IFNULL(group_type_price.min_price, 0),
+        IFNULL(subgroup_type_price.min_price, 0),
         'mrp',
-        IFNULL(group_type_price.mrp, 0),
+        IFNULL(subgroup_type_price.mrp, 0),
         'price',
         IFNULL(subgroup_subtype_price.price, 0),
         'discount',
@@ -56,14 +117,7 @@ JSON_ARRAYAGG(
 ) AS price_details
 FROM
     jaysan_model_subtype
-LEFT JOIN group_type_price ON jaysan_model_subtype.mtid = group_type_price.mtid AND group_type_price.group_id =(
-    SELECT
-        group_id
-    FROM
-        customer_subgroup_master
-    WHERE
-        sub_group_id = $subgroup_id
-)
+LEFT JOIN subgroup_type_price ON jaysan_model_subtype.mtid = subgroup_type_price.mtid 
 LEFT JOIN subgroup_subtype_price ON jaysan_model_subtype.msid = subgroup_subtype_price.msid AND subgroup_subtype_price.sub_group_id = $subgroup_id
 WHERE
     jaysan_model_subtype.mtid = $mtid
@@ -71,7 +125,8 @@ GROUP BY
     subtype_group_id
 ORDER BY
     subtype_group_id
-DESC";
+DESC;
+SQL;
 
 $result = $conn->query($sql);
 
--- query/New_Query_1770721165681.sql
+++ query/New_Query_1770721165681.sql
@@ -1,6 +1,50 @@
-SELECT if((SELECT 1 FROM bom_correction WHERE outpart_bom_id = 10 and part_id = 1316) IS NOT NULL, '10', (SELECT COUNT(bom_id) from bom_output bo WHERE bo.part_id = 1316 and bo.component_cat <> "Process"));
-
-
-SELECT if((SELECT 1 FROM bom_correction WHERE outpart_bom_id = 10 and part_id = 1316) IS NOT NULL, ((SELECT bomlist_id FROM bom_correction WHERE outpart_bom_id = 10 and part_id = 1316)), (SELECT bom_id from bom_output bo WHERE bo.part_id = 1316 and bo.component_cat <> "Process" limit 1) );
-
-SELECT * FROM bom_input left join bom_correction on bom_input.part_id = bom_correction.part_id and bom_correction.outpart_bom_id = 10 where bom_input.bom_id = 10;
+SELECT
+    subtype_group_id,
+ 
+    IFNULL(subgroup_type_price.max_price, 0) AS max_price,
+    IFNULL(subgroup_type_price.min_price, 0) AS min_price,
+    IFNULL(subgroup_type_price.mrp, 0) AS mrp,
+    (
+    SELECT
+        dep_section.sec_name
+    FROM
+        dep_section
+    WHERE
+        dep_section.dep_sec_id = subtype_group_id
+) AS sec_name,
+JSON_ARRAYAGG(
+    JSON_OBJECT(
+        'msid',
+        jaysan_model_subtype.msid,
+        'subtype_name',
+        subtype_name,
+        'is_default',
+        is_default,
+        'is_reduce',
+        is_reduce,
+        'max_price',
+        IFNULL(subgroup_type_price.max_price, 0),
+        'min_price',
+        IFNULL(subgroup_type_price.min_price, 0),
+        'mrp',
+        IFNULL(subgroup_type_price.mrp, 0),
+        'price',
+        IFNULL(subgroup_subtype_price.price, 0),
+        'discount',
+        IFNULL(
+            subgroup_subtype_price.discount,
+            0
+        )
+    )
+) AS price_details
+FROM
+    jaysan_model_subtype
+LEFT JOIN subgroup_type_price ON jaysan_model_subtype.mtid = subgroup_type_price.mtid 
+LEFT JOIN subgroup_subtype_price ON jaysan_model_subtype.msid = subgroup_subtype_price.msid AND subgroup_subtype_price.sub_group_id = $subgroup_id
+WHERE
+    jaysan_model_subtype.mtid = $mtid
+GROUP BY
+    subtype_group_id
+ORDER BY
+    subtype_group_id
+DESC;
