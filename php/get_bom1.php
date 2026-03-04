6477f7ce61b62a7698724ab18a9ee4604f0b5877
nklharish1 <nklharish1@gmail.com>
2026-03-04 19:22:43 +0530

    get bom modified

--- php/get_bom1.php
+++ php/get_bom1.php
@@ -49,8 +49,9 @@ return $data;
             bi.bom_in_id as bom_in_id,  
             bi.part_id AS input_part,
             bi.qty*h.qty AS qty,
+             bi.sub_ass_qty,
             pt.sub_ass,
-            bi.sub_ass_qty,
+           
             h.level + 1,
             boc.component_cat,
         CAST(CONCAT(h.path, '>', (SELECT part_name FROM parts_tbl WHERE part_id = boc.part_id)) AS VARCHAR(500))
@@ -98,6 +99,78 @@ if ($result->num_rows > 0) {
   echo "0 result";
 }
 $conn->close();
+
+$sql = <<<SQL
+WITH RECURSIVE bom_hi AS (
+
+        /* ========= Anchor ========= */
+        SELECT
+        bo.bom_id,
+        pt_hi.part_name as inpart_name,
+            bo.part_id AS output_part,
+            part_out.part_name AS output_part_name,
+            bi.bom_in_id as bom_in_id,
+            bi.part_id AS input_part,
+            bi.qty,
+            bi.sub_ass_qty,
+            pt_hi.sub_ass,
+            0 AS level,
+            bo.component_cat,
+         CAST((SELECT part_name FROM parts_tbl WHERE part_id = bo.part_id) AS CHAR) AS path
+        FROM bom_output bo
+        JOIN bom_input bi ON bo.bom_id = bi.bom_id
+        inner join parts_tbl part_out on bo.part_id = part_out.part_id
+        JOIN parts_tbl pt_hi ON bi.part_id = pt_hi.part_id
+        WHERE bo.bom_id  = (SELECT bo_get.bom_id from bom_output bo_get WHERE bo_get.part_id =   16287 and bo_get.component_cat = "sub assembly" and bo_get.component_cat <> 'Process' )
+        UNION ALL
+
+        /* ========= Recursive ========= */
+        SELECT
+        boc.bom_id,
+         ptc.part_name as inpart_name,
+            boc.part_id AS output_part,
+            part_out.part_name AS output_part_name,
+            bic.bom_in_id as bom_in_id,  
+            bic.part_id AS input_part,
+            bic.qty*h.qty AS qty,
+            bic.sub_ass_qty,
+            bic.sub_ass,
+            
+            h.level + 1,
+            boc.component_cat,
+        CAST(CONCAT(h.path, '>', (SELECT part_name FROM parts_tbl WHERE part_id = boc.part_id)) AS VARCHAR(500))
+
+
+        FROM bom_output boc
+        inner JOIN bom_hi h
+            ON boc.part_id = h.input_part
+        AND h.sub_ass = 1
+        inner JOIN bom_input bic ON boc.bom_id = bic.bom_id
+        inner join parts_tbl part_out on boc.part_id = part_out.part_id
+        inner JOIN parts_tbl ptc ON bic.part_id = ptc.part_id
+        WHERE boc.component_cat <> 'Process'
+        AND boc.part_id <> h.output_part
+   )
+   SELECT
+      output_part,
+        inpart_name,
+     bom_in_id,
+    input_part,
+      qty,
+        sub_ass_qty,
+        sub_ass,
+        level,
+        component_cat,
+        path,
+      qty - sub_ass_qty
+   
+   output_part,
+   output_part_name,
+   bom_id,
+   level
+   FROM bom_hi
+  ;
+SQL;
 
  ?>
 
