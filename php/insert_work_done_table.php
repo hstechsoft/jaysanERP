82ab9ad5f3175e78055ce011230b0622c201e2ea
nklharish1 <nklharish1@gmail.com>
2026-03-17 10:50:21 +0530

    insert work done modifed

---
+++ php/insert_work_done_table.php
@@ -0,0 +1,30 @@
+<?php
+ include 'db_head.php';
+
+
+$emp_id = test_input($_GET['emp_id']);
+
+
+ 
+ 
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+$data = "'".$data."'";
+return $data;
+}
+
+
+ $sql = "INSERT INTO work_done_table (emp_id,start_date) VALUES ($emp_id, NOW())";
+
+  if ($conn->query($sql) === TRUE) {
+   echo "ok";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+$conn->close();
+
+ ?>
+
+
