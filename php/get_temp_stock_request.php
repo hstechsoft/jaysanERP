9511f699028860ccf7d9893060145c01383f94fe
nklharish1 <nklharish1@gmail.com>
2026-04-07 15:57:19 +0530

    stock requset added

--- php/get_temp_stock_request.php
+++ php/get_temp_stock_request.php
@@ -14,7 +14,15 @@ return $data;
 }
 
 
- $sql = "SELECT * FROM stock_request";
+ $sql = "SELECT stock_request.*,parts_tbl.part_name, jaysan_process.process_name, creditors.creditor_name, department.dep_name, dep_section.sec_name, employee.emp_name  FROM stock_request
+ left join parts_tbl on parts_tbl.part_id = stock_request.part_id
+ left join process_wel_tbl on process_wel_tbl.process_id = stock_request.previous_process_id
+ left join jaysan_process on process_wel_tbl.process = jaysan_process.process_id
+ left join creditors on creditors.creditor_id = stock_request.godown
+ left join department on department.dep_id = stock_request.dep
+left join dep_section on dep_section.dep_sec_id = stock_request.sec
+left join employee on employee.emp_id = stock_request.emp_id
+";
 
 $result = $conn->query($sql);
 
