7e5462a7801aa4ab1417e83030b2fdf2ae70ac2c
nklharish1 <nklharish1@gmail.com>
2026-04-09 18:21:18 +0530

    work done updated

--- php/get_employee_work_report.php
+++ php/get_employee_work_report.php
@@ -1,10 +1,9 @@
 <?php
  include 'db_head.php';
 
- $emp_id = test_input($_GET['emp_id']);
- $start_time = test_input($_GET['start_time']);
- $end_time = test_input($_GET['end_time']);
+$work_done_id = test_input($_GET['work_done_id']);
 
+$result_json = array();
  
  
 function test_input($data) {
@@ -16,9 +15,22 @@ return $data;
 }
 
 
+    
+    $total_qr_time = 0;
 
 
- $sql = "WITH qr_summary_wob as (SELECT wd.work_id,wd.start_date,wd.end_date, qr_work_entry.qr_work_id,qr_work_entry.emp_id, qr_work_entry.start_time, qr_work_entry.end_time,qr_work_entry.free_time,qr_work_entry.production_id,qr_work_entry.reason,qr_work_entry.work_sts,
+// get total break time for the current work done id on;y unfinished entry
+$get_total_qr_unfinished = "SELECT ifnull(sum(TIMESTAMPDIFF(MINUTE, start_time, end_time)), 0) as total_qr_time FROM qr_work_entry 
+
+WHERE production_id > 0 and end_time is not null and work_done_id = $work_done_id and production_id not in (select production_id from qr_work_entry where work_done_id = $work_done_id and production_id > 0 and work_sts = 'finished' and end_time is not null)";
+$result_qr_unfinished = $conn->query($get_total_qr_unfinished);
+if ($result_qr_unfinished->num_rows > 0) {
+    while($row = $result_qr_unfinished->fetch_assoc()) {
+        $total_qr_time += $row['total_qr_time']; // Convert minutes to seconds
+    }
+}
+$result_json['total_qr_time'] = $total_qr_time;
+$sql_report = "WITH qr_summary_wob as (SELECT wd.start_date,wd.end_date,qr_work_entry.qr_work_id,qr_work_entry.emp_id, qr_work_entry.start_time, qr_work_entry.end_time,qr_work_entry.free_time,qr_work_entry.production_id,qr_work_entry.reason,qr_work_entry.work_sts,
 JSON_ARRAYAGG(JSON_OBJECT('part_id',work_process.part_id,'qty',work_process.qty,'work_time_per_unit',work_process.work_time_per_unit,'total_time',work_process.qty * work_process.work_time_per_unit,'process_id',work_process.process_id,'process_name',jaysan_process.process_name,'part_name',parts_tbl.part_name)) as process_data,
 
 sum(work_process.qty * work_process.work_time_per_unit) as total_process_time,
@@ -32,37 +44,83 @@ if(pv.production_id>0,JSON_OBJECT('worked_process_data',pv.worked_process_data,'
 FROM qr_work_entry
 
     left join work_process on qr_work_entry.qr_work_id = work_process.current_work_id
-
+   
     left join process_wel_tbl on work_process.process_id = process_wel_tbl.process_id
     left join jaysan_process on process_wel_tbl.process = jaysan_process.process_id
     LEFT join parts_tbl on work_process.part_id = parts_tbl.part_id
-
+   
     left join production_details_view pv on qr_work_entry.production_id = pv.production_id
-    inner join work_done_table wd on qr_work_entry.work_done_id = wd.work_id
+     inner join work_done_table wd on qr_work_entry.work_done_id = wd.work_id
     WHERE
-   qr_work_entry.work_done_id in (select work_id from work_done_table where emp_id = $emp_id and start_date >= '$start_time' and (end_date <= '$end_time' or end_date is null) group by qr_work_entry.qr_work_id)  group by qr_work_entry.qr_work_id),
-    qr_summary as ( SELECT qr_summary_wob.*,JSON_ARRAYAGG(JSON_OBJECT('break_time',work_break.break_time,'ext_id',work_break.ext_id,'ex_name',extra_time_master.ex_name)) as break_data,
+   qr_work_entry.work_done_id = $work_done_id group by qr_work_entry.qr_work_id),
 
-sum(work_break.break_time) as total_break_time from  qr_summary_wob
+ qr_summary as ( SELECT qr_summary_wob.*,
+ JSON_ARRAYAGG(
+  CASE 
+    WHEN extra_time_master.ex_type = 'break' 
+    THEN JSON_OBJECT('break_time',work_break.break_time,'ext_id',work_break.ext_id,'ex_name',extra_time_master.ex_name)
+  END
+) as break_data,
+
+JSON_ARRAYAGG(
+  CASE 
+    WHEN extra_time_master.ex_type != 'break' 
+    THEN JSON_OBJECT('break_time',work_break.break_time,'ext_id',work_break.ext_id,'ex_name',extra_time_master.ex_name)
+  END
+) as extra_work_data,
+
+
+ 
+SUM(CASE WHEN extra_time_master.ex_type = 'break' THEN work_break.break_time ELSE 0 END) as total_break_time,
+SUM(CASE WHEN extra_time_master.ex_type != 'break' THEN work_break.break_time ELSE 0 END) as total_extra_work_time
+ from  qr_summary_wob
   left join work_break on qr_summary_wob.qr_work_id = work_break.current_work_id
-   left join extra_time_master on work_break.ext_id = extra_time_master.ext_id GROUP BY qr_summary_wob.qr_work_id
+   left join extra_time_master on work_break.ext_id = extra_time_master.ext_id    GROUP BY qr_summary_wob.qr_work_id
   )
-SELECT work_id,start_date, end_date,TIMESTAMPDIFF(MINUTE, start_date, end_date) as total_work_duration,sum(free_time) as total_free_time,sum(total_time) as total_work_time,sum(total_process_time) as total_process_time,sum(total_break_time) as total_break_time,sum(total_processes) as total_process_count, JSON_ARRAYAGG(JSON_OBJECT('qr_work_id', qr_work_id, 'emp_id', emp_id, 'start_time', start_time, 'end_time', end_time, 'free_time', free_time, 'production_id', qr_summary.production_id, 'reason', reason, 'work_sts', work_sts, 'process_data', if(qr_summary.production_id>0,worked_process_data,process_data), 'break_data', if(qr_summary.production_id>0,null,break_data), 'total_process_time', total_process_time, 'total_break_time', total_break_time, 'total_time', total_time, 'total_processes', total_processes, 'production_data', production_data, 'assign_product_data', if(ap.ass_id>0, JSON_OBJECT('dated',ap.dated,'emergency_order',ap.emergency_order,'chasis_no',ap.chasis_no), null))) as detailed_data  FROM qr_summary 
+SELECT qr_work_id,start_date, end_date,TIMESTAMPDIFF(MINUTE, start_date, end_date) as total_work_duration, emp_id, start_time, end_time, free_time, qr_summary.production_id, reason, work_sts, if(qr_summary.production_id>0,worked_process_data,process_data) as process_data, if(qr_summary.production_id>0,null,break_data) as break_data,if(qr_summary.production_id>0,null,extra_work_data) as extra_work_data, total_process_time, total_break_time, total_extra_work_time, total_time, total_processes,production_data, if(ap.ass_id>0, JSON_OBJECT('dated',ap.dated,'emergency_order',ap.emergency_order,'chasis_no',ap.chasis_no), null) as assign_product_data FROM qr_summary 
 left  join machine_production_taken mpt on qr_summary.production_id = mpt.production_id
-left join assign_product ap on mpt.ass_id = ap.ass_id GROUP BY work_id";
+left join assign_product ap on mpt.ass_id = ap.ass_id";
+$result_report = $conn->query($sql_report);
+  $rows = array();
+    $total_process_entry_time = 0;
+    $total_break_entry_time = 0;
+    $tpt = 0;
+    $total_free_time = 0;
+    $total_extra_work_time = 0;
+if ($result_report->num_rows > 0) {
+    while($row = $result_report->fetch_assoc()) {
+        $rows[] = $row;
+        $total_process_entry_time += $row['total_time'];
+        $total_break_entry_time += $row['total_break_time'];
+        $tpt += $row['total_process_time'];
+        $total_free_time += $row['free_time'];
+        $total_work_duration = $row['total_work_duration'];
+        $total_extra_work_time += $row['total_extra_work_time'];
 
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    $rows = array();
-    while($r = mysqli_fetch_assoc($result)) {
-        $rows[] = $r;
     }
-    print json_encode($rows);
-} else {
-  echo "0 result";
-}
+} 
+     else {
+    echo "Error: " . $sql_report . "<br>" . $conn->error;
+  }
+
+
+
+
+
+
+
+$result_json['report'] = $rows;
+$result_json['total_day_time'] = $total_work_duration;
+$result_json['paused_time'] = $total_qr_time;
+$result_json['break_time'] = $total_break_entry_time;
+$result_json['total_process_time'] = $tpt;
+$result_json['total_extra_work_time'] = $total_extra_work_time;
+$total_free_time = 0;
+$total_free_time = $total_work_duration - ( $total_break_entry_time + $total_qr_time + $tpt+ $total_extra_work_time);
+$result_json['free_time'] = $total_free_time;
+print json_encode($result_json);
 $conn->close();
 
  ?>
+
 
--- php/get_work_done_recent.php
+++ php/get_work_done_recent.php
@@ -1,33 +1,26 @@
 <?php
  include 'db_head.php';
 
- $emp_id = test_input($_GET['emp_id']);
+ $emp_id = test_input($_GET['emp_id']) ? test_input($_GET['emp_id']) : 0;
+ $start_date = test_input($_GET['start_date']) ? test_input($_GET['start_date']) : null;
+ $end_date = test_input($_GET['end_date']) ? test_input($_GET['end_date']) : null;
+$emp_id_query = 1;
+$date_query = 1;
 
+ if($emp_id > 0)
+  $emp_id_query = "work_done_table.emp_id = $emp_id";
 
- 
+ if($start_date && $end_date)
+  $date_query = "work_done_table.start_date >= '$start_date' AND work_done_table.end_date <= '$end_date'";
  
 function test_input($data) {
 $data = trim($data);
 $data = stripslashes($data);
 $data = htmlspecialchars($data);
-$data = "'".$data."'";
+
 return $data;
 }
-if($emp_id =="'0'")
-{
-  $sql =  "SELECT work_done_table.work_id, work_done_table.emp_id, 
-  start_date as start_date_1,
-                  end_date as end_date_1,
-                  DATE_FORMAT(work_done_table.start_date, '%d-%m-%Y %r') as start_date, 
-                  DATE_FORMAT(work_done_table.end_date, '%d-%m-%Y %r') as end_date, 
-                  employee.emp_name 
-           FROM `work_done_table` 
-           INNER JOIN employee ON employee.emp_id = work_done_table.emp_id 
-           ORDER BY work_done_table.start_date DESC 
-           LIMIT 10";
-}
-else
-{
+
  $sql =  "SELECT work_done_table.work_id, work_done_table.emp_id, 
   start_date as start_date_1,
                   end_date as end_date_1,
@@ -35,8 +28,8 @@ else
                   DATE_FORMAT(work_done_table.end_date, '%d-%m-%Y %r') as end_date, 
                   employee.emp_name 
            FROM `work_done_table` 
-           INNER JOIN employee ON employee.emp_id = work_done_table.emp_id WHERE work_done_table.emp_id =  $emp_id order by work_done_table.start_date desc limit 10";
-}
+           INNER JOIN employee ON employee.emp_id = work_done_table.emp_id WHERE $emp_id_query and $date_query and end_date is not null order by work_done_table.start_date desc limit 10";
+
 
 
 $result = $conn->query($sql);
