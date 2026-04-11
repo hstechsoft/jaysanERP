b5bb06b5499a3cefc7d3fceb6bba98e107398182
nklharish1 <nklharish1@gmail.com>
2026-04-11 12:25:37 +0530

    process updated

--- php/update_work_time_master1.php
+++ php/update_work_time_master1.php
@@ -3,13 +3,14 @@
 
  $min_time = test_input($_POST['min_time']);
 $max_time = test_input($_POST['max_time']);
-$process_id = test_input($_POST['process_id']);
-$machine_id = test_input($_POST['machine_id']);
-$dep_id = test_input($_POST['dep_id']);
-$dep_sec_id = test_input($_POST['dep_sec_id']);
+$process_id = (test_input($_POST['process_id']));
+$machine_id = sql_nullable(test_input($_POST['machine_id']));
+$dep_id = sql_nullable(test_input($_POST['dep_id']));
+$dep_sec_id = sql_nullable(test_input($_POST['dep_sec_id']));
 $cost = test_input($_POST['cost']);
-$godown_id = test_input($_POST['godown_id']);
-$ori_process_id = test_input($_POST['ori_process_id']);
+$godown_id = sql_nullable(test_input($_POST['godown_id']));
+$ori_process_id = sql_nullable(test_input($_POST['ori_process_id']));
+$wtid  = test_input($_POST['wtid']);
 
 
  
@@ -18,9 +19,21 @@ function test_input($data) {
 $data = trim($data);
 $data = stripslashes($data);
 $data = htmlspecialchars($data);
-$data = "'".$data."'";
+
 return $data;
 }
+
+if($wtid == "0")
+  {
+  $sql =  "INSERT INTO work_time_master (min_time,max_time,process_id,machine_id,dep_id,dep_sec_id,cost,godown_id,ori_process_id) VALUES ($min_time,$max_time,$process_id,$machine_id,$dep_id,$dep_sec_id,$cost,$godown_id,$ori_process_id)";
+  if ($conn->query($sql) === TRUE) {
+   echo "ok";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  }
+  else
+  {
 
 
  $sql =  "UPDATE  work_time_master SET min_time =  $min_time,max_time =  $max_time,process_id =  $process_id,machine_id =  $machine_id,dep_id =  $dep_id,dep_sec_id =  $dep_sec_id,cost =  $cost,godown_id =  $godown_id,ori_process_id =  $ori_process_id WHERE wtid =  $wtid";
@@ -29,6 +42,7 @@ return $data;
    echo "ok";
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
+  }
   }
 $conn->close();
 
