b3360fab79438b96ad8a1702c1a852f7f174c0cf
nklharish1 <nklharish1@gmail.com>
2026-04-11 16:02:31 +0530

    pre process updated

--- php/add_wel_process.php
+++ php/add_wel_process.php
@@ -39,6 +39,13 @@ if ($conn->query($insert_process_sql) === TRUE) {
     exit();
 }
 
+// insert input_wel_parts for new process id
+$insert_input_wel_parts_sql = "insert into input_wel_parts (previous_process_id,input_part_id,qty,process_id) values ($process_id,null,1,$new_process_id);";
+if ($conn->query($insert_input_wel_parts_sql) === TRUE) {
+} else {
+    echo "Error: " . $insert_input_wel_parts_sql . "<br>" . $conn->error;
+}
+
 // update cat = '' and output_part = null for old process id
 $update_old_process_sql = "update process_wel_tbl set cat = '', output_part = null where process_id = $process_id;";
 echo $update_old_process_sql;
@@ -50,7 +57,7 @@ if ($conn->query($update_old_process_sql) === TRUE) {
 }
 
 // 2nd update all process which have previous_process_id = deleted process_id to previous_process_id = previous_process_id of deleted process_id
-$update_previous_process_id_sql = "update process_wel_tbl set previous_process_id = $new_process_id where previous_process_id = $process_id;";
+$update_previous_process_id_sql = "update process_wel_tbl set previous_process_id = $new_process_id where previous_process_id = $process_id and process_id != $new_process_id;";
 echo $update_previous_process_id_sql;
 if ($conn->query($update_previous_process_id_sql) === TRUE) {
 
