4b678ea989f776e1c42849aa484fc2d134268d80
nklharish1 <nklharish1@gmail.com>
2026-04-11 12:46:59 +0530

    pre process updated

---
+++ php/update_input_wel_parts_pre_process.php
@@ -0,0 +1,35 @@
+<?php
+ include 'db_head.php';
+
+
+ $id =test_input($_GET['id']);
+
+ $previous_process_id = sql_nullable(test_input($_GET['previous_process_id']));
+ 
+
+
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+
+return $data;
+}
+
+$sql = "UPDATE process_wel_tbl SET previous_process_id = $previous_process_id WHERE id = $id;";
+
+  if ($conn->query($sql) === TRUE) {
+    echo "ok";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+    
+  
+
+
+
+$conn->close();
+
+ ?>
+
+
