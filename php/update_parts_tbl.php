55994dd69e7a35834ee7390d174b058503f4a1b8
nklharish1 <nklharish1@gmail.com>
2026-05-09 15:42:12 +0530

    parts tbl updated

--- php/update_parts_tbl.php
+++ php/update_parts_tbl.php
@@ -31,7 +31,7 @@ $gstdetails = test_input($_POST['gstdetails']);
 $type_of_supply = test_input($_POST['type_of_supply']);
 $part_id = test_input($_POST['part_id']);
 
-
+echo $item_grade;
  
  
 function test_input($data) {
