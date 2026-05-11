c00c2172dc61de1a813b2c62181c6bf89dd077ff
nklharish1 <nklharish1@gmail.com>
2026-05-11 12:59:33 +0530

    sales payment updated

--- php/get_sales_payment.php
+++ php/get_sales_payment.php
@@ -19,7 +19,7 @@ return $data;
         '₹', FORMAT(jaysan_payment.amount, 0)
     ) as amount, DATE_FORMAT(
         jaysan_payment.payment_date, '%d-%m-%Y %h:%i %p'
-    ) as date_f, jaysan_payment.sts, sales_order_form.order_no,jaysan_payment.ref_no,jaysan_payment.utr_no
+    ) as date_f, jaysan_payment.sts, sales_order_form.order_no,jaysan_payment.ref_no,jaysan_payment.utr_no,'payment' as pay_sts 
 from jaysan_payment
     inner JOIN sales_order_form on jaysan_payment.oid = sales_order_form.oid
 WHERE
@@ -33,7 +33,7 @@ SELECT concat(
         '₹', FORMAT(sale_payment_advance.amount, 0)
     ) as amount, DATE_FORMAT(
         sale_payment_advance.dated, '%d-%m-%Y %h:%i %p'
-    ) as date_f, concat('advance taken from - ', sf1.order_no) as sts, sales_order_form.order_no,jaysan_payment.ref_no,jaysan_payment.utr_no
+    ) as date_f, concat('advance taken from - ', sf1.order_no) as sts, sales_order_form.order_no,jaysan_payment.ref_no,jaysan_payment.utr_no,'advance' as pay_sts
 from sale_payment_advance 
     inner JOIN sales_order_form on sale_payment_advance.oid = sales_order_form.oid and advance_ref_id > 0
     inner join sale_payment_advance s1 on sale_payment_advance.advance_ref_id = s1.advance_id
