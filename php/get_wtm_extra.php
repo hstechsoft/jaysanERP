54a390a1dbe50c2f398108a954f1aaccc85d80c9
nklharish1 <nklharish1@gmail.com>
2026-05-25 12:48:23 +0530

    get extra added

--- php/get_wtm_extra.php
+++ php/get_wtm_extra.php
@@ -20,7 +20,7 @@ return $data;
 
 
 $sql = <<<SQL
-with mrf_details as(SELECT  mrf_purchase.raw_material_part_id, group_concat(mrf_purchase.raw_material_rate) as approx_delivery_days, creditors.creditor_name, creditors.creditor_id FROM mrf_purchase
+with mrf_details as(SELECT  mrf_purchase.raw_material_part_id, group_concat(mrf_purchase.raw_material_rate) as raw_material_rate, creditors.creditor_name, creditors.creditor_id FROM mrf_purchase
 inner join creditors on mrf_purchase.po_order_to = creditors.creditor_id GROUP BY raw_material_part_id,creditors.creditor_id),
 wtm as (select godown_id,output_part from process_wel_tbl inner join work_time_master on process_wel_tbl.process_id = work_time_master.ori_process_id where process_wel_tbl.component_cat = 'purchase' and process_wel_tbl.process_title = 'purchase' and process_wel_tbl.cat = 'out')
 select raw_material_part_id, approx_delivery_days, creditor_name, creditor_id, godown_id from mrf_details 
