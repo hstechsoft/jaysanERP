ed9d6a730947504927b7970996ae52ec6a7fa9c6
nklharish1 <nklharish1@gmail.com>
2026-06-03 16:10:30 +0530

    corrected

--- php/get_current_work_break_admin.php
+++ php/get_current_work_break_admin.php
@@ -60,7 +60,7 @@ WHERE TIMESTAMP(d.dt, b.start_time) >= '$start_time'
 $result = $conn->query($sql);
 
 
-echo "SQL Query: " . $sql . "\n"; // Debugging line to check the generated SQL query
+// echo "SQL Query: " . $sql . "\n"; // Debugging line to check the generated SQL query
 if ($result->num_rows > 0) {
     $rows = array();
     while($r = mysqli_fetch_assoc($result)) {
