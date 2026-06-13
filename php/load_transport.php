0cad624fa64dd2b6a33234aa386fa6ab836f7c70
nklharish1 <nklharish1@gmail.com>
2026-06-13 17:23:09 +0530

    corrected

--- php/load_transport.php
+++ php/load_transport.php
@@ -44,27 +44,43 @@ if ($qty > $stock_qty) {
             throw new Exception("Error updating stock reserve id $stock_reserve_id: " . $conn->error);
         }
 
-        // update stock to transport godown on duplicate key update
-         $sql_update_stock = "INSERT INTO jaysan_stock (part_id, process_id, godown, qty,batch_id) VALUES ($part_id, $process_id, $transport_godown, $qty, '$batch_id') ON DUPLICATE KEY UPDATE qty = qty + $qty";
-      //  get updated stock id
-        if ($conn->query($sql_update_stock) === TRUE) {
+        // check if stock with same part id, process id, godown and batch id exists in transport godown
+        $sql_check_stock = "SELECT stock_id FROM jaysan_stock WHERE part_id = $part_id AND process_id = $process_id AND godown = $transport_godown AND batch_id = $batch_id";
+        $result_check_stock = $conn->query($sql_check_stock);
+        if ($result_check_stock->num_rows > 0) {
+            // stock exists in transport godown, update quantity
+            $row_check_stock = $result_check_stock->fetch_assoc();
+            $existing_stock_id = $row_check_stock['stock_id'];
+        } else {
+            $existing_stock_id = null;
+        }
+
+
+        // if stock exists in transport godown update stock with new quantity else insert new stock with quantity
+        if($existing_stock_id) {
+            $sql_update_stock = "UPDATE jaysan_stock SET qty = qty + $qty WHERE stock_id = $existing_stock_id";
+            if (!$conn->query($sql_update_stock)) {
+                throw new Exception("Error updating stock id $existing_stock_id in transport godown: " . $conn->error);
+            }
+            $new_stock_id = $existing_stock_id;
+        } else {
+
+
+        // insert new stock in transport godown with quantity and get new stock id
+        $sql_insert_stock = "INSERT INTO jaysan_stock (part_id, process_id, godown, qty,batch_id) VALUES ($part_id, $process_id, $transport_godown, $qty, '$batch_id')";
+        if ($conn->query($sql_insert_stock) === TRUE) {
             $new_stock_id = $conn->insert_id;
-            if ($new_stock_id == 0) {
-                // if duplicate key update happened get stock id of transport godown
-                $sql_get_stock_id = "SELECT stock_id FROM jaysan_stock WHERE part_id = $part_id AND process_id = $process_id AND godown = $transport_godown AND batch_id = $batch_id";
-                $result_get_stock_id = $conn->query($sql_get_stock_id);
-                if ($result_get_stock_id->num_rows > 0) {
-                    $row_get_stock_id = $result_get_stock_id->fetch_assoc();
-                    $new_stock_id = $row_get_stock_id['stock_id'];
-                } else {
-                    throw new Exception("Error getting new stock id for part id $part_id and process id $process_id in transport godown: " . $conn->error);
-                }
+        } else {
+            throw new Exception("Error inserting new stock in transport godown for part id $part_id and process id $process_id: " . $conn->error);
+        }   
 
 
-          
-        } 
+        
+ 
 
-
+        }   
+        
+        
         // using that new_stock_id update stock reserve with new stock id and reserve qty
                 $sql_update_reserve = "UPDATE stock_reserve SET stock_id = $new_stock_id, reserve_qty = $qty WHERE stock_reserve_id = $stock_reserve_id";
                 if (!$conn->query($sql_update_reserve)) {
@@ -72,9 +88,9 @@ if ($qty > $stock_qty) {
                 }
 
        
-    } else {
-        throw new Exception("Error getting stock details for stock reserve id $stock_reserve_id: " . $conn->error.$sql_update_stock);
-    }
+
+
+
 
     // update current transport in transport parts with $transport_godown and sts as transport
     $sql_update_transport = "UPDATE transport_parts SET current_transport = $transport_godown, sts = 'transport' WHERE reserve_id = $stock_reserve_id";
