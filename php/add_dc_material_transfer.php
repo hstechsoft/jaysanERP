10f3c10550f804fb1303f65a98a0108cacab4af5
nklharish1 <nklharish1@gmail.com>
2026-06-16 16:35:57 +0530

    modified

--- php/add_dc_material_transfer.php
+++ php/add_dc_material_transfer.php
@@ -16,6 +16,7 @@ $data = trim($data);
 $data = stripslashes($data);
 $data = htmlspecialchars($data);
 $data = "'".$data."'";
+
 return $data;
 }
 
@@ -35,6 +36,7 @@ $sql_get_transport = "SELECT transport_dc_id FROM `transport_dc` WHERE source_go
 
             if($transport_dc_id == 0)
               {
+                
                      $sql_in_dc = "INSERT INTO transport_dc (source_godown,des_godown,transport_godown,dc_id) VALUES ($source_godown, $des_godown, $current_transport, $dc_id)";
                     // get in_dc_tracking id
                     if ($conn->query($sql_in_dc) === TRUE) {
@@ -43,7 +45,150 @@ $sql_get_transport = "SELECT transport_dc_id FROM `transport_dc` WHERE source_go
                         throw new Exception("Error inserting in_dc_tracking: " . $conn->error);
                     }
               }
-         
+
+            //   get delivery challan details
+            $sql_get_addr = "SELECT (SELECT concat(creditor_name ,' ',
+    creditor_phone,' ',
+    creditors_addr,' ',
+    creditor_gst)
+FROM creditors WHERE creditor_id = $des_godown) as ship_to,
+(SELECT company_address
+ FROM company) as company";
+
+  $result_get_addr = $conn->query($sql_get_addr);
+        if ($result_get_addr->num_rows > 0) {
+            // stock exists in transport godown, update quantity
+            $row_get_addr = $result_get_addr->fetch_assoc();
+            $ship_to = $row_get_addr['ship_to'];
+            $company = $row_get_addr['company'];
+
+        } 
+
+
+
+
+ $sql = "INSERT INTO delivery_challan ( dc_date, dc_type, dc_from, dc_to, bill_to, ship_to) VALUES (current_timestamp(),)";
+    if ($conn->query($sql) === TRUE) {
+        $dc_id = $conn->insert_id;
+// insert dc parts
+        foreach ($dc_parts as $part) {
+            $part_id = test_input($part['part_id']);
+              $part_pre_process_id  = test_input($part['part_pre_process_id']);
+                $rate = test_input($part['rate']);
+            $qty = test_input($part['qty']);
+            $sql_part = "INSERT INTO dc_parts (dc_id, part_id, part_pre_process_id, rate, qty) VALUES ($dc_id, $part_id, $part_pre_process_id, $rate, $qty)";
+            if (!$conn->query($sql_part)) {
+                throw new Exception("Error inserting part: " . $conn->error);
+            }
+        }
+
+// insert dc process
+        foreach ($dc_process as $process) {
+            $process_id = test_input($process['process_id']);
+            $qty = test_input($process['qty']);
+            $rate = test_input($process['rate']);
+            
+            $sql_process = "INSERT INTO dc_process (dc_id, process_id, qty, rate) VALUES ($dc_id, $process_id, $qty, $rate)";
+            if (!$conn->query($sql_process)) {
+                throw new Exception("Error inserting process: " . $conn->error);
+            }
+        }
+
+
+        // reserve stock for dc parts
+        foreach ($dc_parts_location as $location) {
+            $reserve_type = "dc";
+            $reserve_type_id = $dc_id;
+            $emp_id = $emp_id;
+            $remark = "Reserved for DC #".$dc_id;
+            $stock_id = $location['stock_id'];
+            $reserve_qty = $location['qty'];
+              $sql_reserve = "INSERT INTO stock_reserve (reserve_type, reserve_type_id, emp_id, remark, stock_id, reserve_qty) VALUES ('$reserve_type', $reserve_type_id, $emp_id, '$remark', $stock_id, $reserve_qty)";
+            //   get reserve id
+            if ($conn->query($sql_reserve) === TRUE) {
+                $reserve_id = $conn->insert_id;
+            } else {
+                throw new Exception("Error reserving stock: " . $conn->error);
+            }
+          
+            
+
+
+            // if stock taken from other then source godown need to insert in_dc_tracking
+            // check godown of stock id
+            $sql_godown = "SELECT * FROM jaysan_stock WHERE stock_id = $stock_id";
+            $result_godown = $conn->query($sql_godown);
+            if ($result_godown->num_rows > 0) {
+                $row_godown = $result_godown->fetch_assoc();
+                $godown_id = $row_godown['godown'];
+                $stock_process_id = $row_godown['process_id'];
+                $stock_part_id = $row_godown['part_id'];
+                // if ($godown_id != $dc_from) {
+                //     // insert in_dc_tracking
+                //     $sql_in_dc = "INSERT INTO in_dc_tracking (out_dc_id, godown, dated) VALUES ($dc_id, $godown_id, NOW())";
+                //     // get in_dc_tracking id
+                //     if ($conn->query($sql_in_dc) === TRUE) {
+                //         $in_dc_id = $conn->insert_id;
+                //     } else {
+                //         throw new Exception("Error inserting in_dc_tracking: " . $conn->error);
+                //     }
+
+                //     // add parts to in_dc_parts 
+                //     $sql_in_dc_parts = "INSERT INTO in_dc_parts (tracking_id, part_id,process_id, qty) VALUES ($in_dc_id, $stock_part_id, $stock_process_id, $reserve_qty)";
+                //     if (!$conn->query($sql_in_dc_parts)) {
+                //         throw new Exception("Error inserting in_dc_parts: " . $conn->error);
+                //     }
+                // }
+
+
+          
+                    // insert in_dc_tracking
+                    $sql_in_dc = "INSERT INTO transport_dc (source_godown,des_godown,transport_godown,dc_id) VALUES ($godown_id, $dc_to, $transport_godown, $dc_id)";
+                    // get in_dc_tracking id
+                    if ($conn->query($sql_in_dc) === TRUE) {
+                        $transport_dc_id = $conn->insert_id;
+                    } else {
+                        throw new Exception("Error inserting in_dc_tracking: " . $conn->error);
+                    }
+
+                    // add parts to in_dc_parts 
+                    $sql_in_dc_parts = "INSERT INTO  transport_parts (transport_dc_id,part_id,process_id, qty,reserve_id) VALUES ($transport_dc_id, $stock_part_id, $stock_process_id, $reserve_qty, $reserve_id)";
+                    if (!$conn->query($sql_in_dc_parts)) {
+                        throw new Exception("Error inserting in_dc_parts: " . $conn->error);
+                    }
+             
+
+
+
+
+
+            }
+            
+   
+        }
+
+
+           require_once 'print_dc.php';
+            $result = print_dc($dc_id, $conn);
+
+            // // save filepath in delivery challan table
+            // $sql_update = "UPDATE delivery_challan SET file_path = '".$result['pdf_file_name']."' WHERE dc_id = $dc_id";
+            // if (!$conn->query($sql_update)) {
+            //     throw new Exception("Error updating delivery challan: " . $conn->error);
+            // }
+
+//  print array
+        print_r($result);
+        
+        $conn->commit();
+
+// print dc
+
+    } else {
+        throw new Exception("Error: " . $sql . "<br>" . $conn->error);
+    }
+        
+        $conn->commit();
  
 }
  catch (Exception $e) {
