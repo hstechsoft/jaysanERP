cb03df82030f06990af212567ccc434d4c636b1b
nklharish1 <nklharish1@gmail.com>
2026-06-18 17:49:27 +0530

    transpoert part dc added

--- php/insert_dc_trip.php
+++ php/insert_dc_trip.php
@@ -113,7 +113,7 @@ try {
                 //     }
                 // }
 // check if stock is taken from source godown or not
-                if ($godown_id != $source_godown) {
+                if ($godown_id != "'$source_godown'") {
                     throw new Exception("Stock id $stock_id is not from source godown $source_godown");
                 }
 
