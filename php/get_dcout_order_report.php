e2c1430f8fb09f2788f309a95825ec73f62fd3c4
nklharish1 <nklharish1@gmail.com>
2026-06-22 18:28:04 +0530

    dc report added

--- php/get_dcout_order_report.php
+++ php/get_dcout_order_report.php
@@ -23,11 +23,10 @@ function test_input($data) {
   return $data;
 }
 
-$sql = "with transport as(SELECT  transport_dc.*,source.creditor_name as source,dest.creditor_name as destination from transport_dc
+$sql = "SELECT  transport_dc.*,source.creditor_name as source,dest.creditor_name as destination from transport_dc
 left join creditors source on transport_dc.source_godown = source.creditor_id
 left join creditors dest on transport_dc.des_godown = dest.creditor_id
-WHERE $source_query and $destination_query and sts = 'create')
-select des_godown_name,des_godown ,JSON_ARRAYAGG(JSON_OBJECT('godown',godown,'dep',dep,'sec',sec,'creditor_name',creditor_name,'dep_name',dep_name,'sec_name',sec_name,'parts',parts)) as parts from transport";
+WHERE $source_query and $destination_query and sts = 'create'";
 
 $result = $conn->query($sql);
 
