3f5404bcbbf4e24e65c75f3c5ca686c297af44ef
nklharish1 <nklharish1@gmail.com>
2026-06-24 12:54:23 +0530

    work order added

--- php/insert_work_order.php
+++ php/insert_work_order.php
@@ -42,12 +42,15 @@ if (!$conn->query($sql_insert_work_order)) {
 // get parts from input_parts_tbl
 
 $sql_get_parts = "SELECT * FROM input_wel_parts WHERE process_id = $process_id";
+echo "sql_get_parts: $sql_get_parts\n";
 $result = $conn->query($sql_get_parts);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $input_part_id = $row['input_part_id '];
         $previous_process_id = $row['previous_process_id'];
         $qty = $row['qty'];
+
+        echo "input_part_id: $input_part_id, previous_process_id: $previous_process_id, qty: $qty\n";
        
 // get stock id from stock table where process_id = $previous_process_id and input_part_id = $input_part_id
         $sql_get_stock_id = "SELECT stock_id FROM jaysan_stock WHERE process_id = $previous_process_id AND part_id = $input_part_id and godown <=> $godown and dep <=> $dep and sec <=> $sec";
