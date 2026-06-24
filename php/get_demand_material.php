5ab7cc09d451b8d81bcc87e89bef6787f7720844
nklharish1 <nklharish1@gmail.com>
2026-06-24 15:08:42 +0530

    get work order added

--- php/get_demand_material.php
+++ php/get_demand_material.php
@@ -23,7 +23,7 @@ return $data;
 
 //  $sql = "SELECT (SELECT 1 FROM process_wel_tbl WHERE process_wel_tbl.output_part =  parts_tbl.part_id   and cat = 'out')  as process_availble, (select part_name from parts_tbl where part_id = $part_id) as out_part_name,(select sub_ass from parts_tbl where part_id = bom_input.part_id) as sub_ass, bom_input.part_id,bom_input.qty,parts_tbl.part_name,bom_input.bom_id,parts_tbl.part_no from bom_input INNER JOIN bom_output on bom_input.bom_id = bom_output.bom_id INNER JOIN parts_tbl on bom_input.part_id = parts_tbl.part_id WHERE bom_output.part_id =$part_id and bom_output.component_cat = $component_cat";
 
- $sql = "SELECT stock_reserve_view.*,if(stock_reserve_view.part_id is null,concat('semi finished part of',pt_final.part_name),parts_tbl.part_name) as part_name,process_name, stock_reserve_view.qty  - stock_reserve_view.reserve_qty as demand_qty from stock_reserve_view
+ $sql = "SELECT stock_reserve_view.*,if(stock_reserve_view.part_id is null,concat('semi finished part of',pt_final.part_name),parts_tbl.part_name) as part_name,process_name,    stock_reserve_view.reserve_qty - stock_reserve_view.qty as demand_qty from stock_reserve_view
  left join parts_tbl on stock_reserve_view.part_id = parts_tbl.part_id
  left join process_wel_tbl on process_wel_tbl.process_id = stock_reserve_view.process_id
  left join jaysan_process on jaysan_process.process_id = process_wel_tbl.process
