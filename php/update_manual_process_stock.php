6601e208d5cfb6a585e273ac4ad1fad8ca2b13bc
nklharish1 <nklharish1@gmail.com>
2026-07-02 18:04:02 +0530

    demand added

--- php/update_manual_process_stock.php
+++ php/update_manual_process_stock.php
@@ -53,6 +53,7 @@ $sql = "insert into jaysan_stock (godown,dep,sec,process_id,qty,remark,part_id) 
     $stock_id = $conn->insert_id;
    require_once 'stock_distribution.php';
    $result = stock_distribution($conn,$stock_id,$qty,$process_id);
+   echo "<br>Stock distribution result: ".$result;
    if ($result) {
        echo "ok";
    } else {
