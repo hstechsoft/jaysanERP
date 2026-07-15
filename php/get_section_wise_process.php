60f88599bc5491fd02af6dc3854b4f0e2c3a3371
nklharish1 <nklharish1@gmail.com>
2026-07-15 10:20:12 +0530

    corrected

--- php/get_section_wise_process.php
+++ php/get_section_wise_process.php
@@ -24,7 +24,7 @@ return $data;
 
 
  $sql = "with  sec_wise as (SELECT pwt.previous_process_id, pwt.process,pwt.process_id,pwt.process_title,wtm.min_time,wtm.max_time,wtm.dep_id,wtm.dep_sec_id,wtm.godown_id,wtm.wtid FROM work_time_master wtm inner JOIN process_wel_tbl pwt on wtm.ori_process_id = pwt.process_id WHERE  wtm.godown_id = $godown_id and wtm.dep_id = $dep_id and wtm.dep_sec_id = $sec_id and wtm.machine_id = $machine_id), 
-outpart as (SELECT 	get_output_part(process_id) as outpart,  previous_process_id, process,process_id,process_title,dep_id,dep_sec_id,godown_id,wtid FROM sec_wise )
+outpart as (SELECT min_time,max_time, get_output_part(process_id) as outpart,  previous_process_id, process,process_id,process_title,dep_id,dep_sec_id,godown_id,wtid FROM sec_wise )
 SELECT outpart.*,jp.process_name,pt.part_name as output_part FROM outpart
  inner join jaysan_process jp on outpart.process = jp.process_id
  inner join parts_tbl pt on outpart.outpart = pt.part_id";
