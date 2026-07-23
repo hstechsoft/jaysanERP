154917bdfcd10121d45fd13914f91cf99d401d01
nklharish1 <nklharish1@gmail.com>
2026-07-23 12:05:36 +0530

    del modified

--- php/del_wel_process1.php
+++ php/del_wel_process1.php
@@ -3,29 +3,28 @@ include 'db_head.php';
 
 
 
-$did =  $_POST['did'];
+$process_id =  $_POST['process_id'];
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
 
+return $data;
+}
 
-
-
-
-
-foreach ($did as $row) {
-
-
-$sql = "DELETE from process_wel_tbl WHERE process_id = $row" ;
-
-
-log_delete_query($sql);
-
-if ($conn->query($sql) === TRUE) {
-   
+// delete process_wel_tbl
+$sql_delete = "DELETE from process_wel_tbl WHERE final_process_id = $process_id" ;
+if ($conn->query($sql_delete) === TRUE) {
+    echo "ok";
   } else {
-    
+    echo "Error deleting record: " . $conn->error;
   }
 
-}
-echo "ok";
+
+
+
+
+
 
 $conn->close();
 ?>
