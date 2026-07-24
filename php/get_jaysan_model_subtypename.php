5ebe500cd9a6c03c66ab48ac55378655e6cd5030
nklharish1 <nklharish1@gmail.com>
2026-07-24 09:53:44 +0530

    subtype corrected

--- php/get_jaysan_model_subtypename.php
+++ php/get_jaysan_model_subtypename.php
@@ -2,10 +2,17 @@
  include 'db_head.php';
 
  
-if (isset($_GET['product_id'])) {
-    $product_id = test_input($_GET['product_id']);
+// if (isset($_GET['product_id'])) {
+//     $product_id = test_input($_GET['product_id']);
+// } else {
+//     echo "Missing product_id";
+//     exit;
+// }
+
+if (isset($_GET['type_id'])) {
+    $type_id = test_input($_GET['type_id']);
 } else {
-    echo "Missing product_id";
+    echo "Missing type_id";
     exit;
 }
  
@@ -19,7 +26,9 @@ return $data;
 }
 
 
- $sql = "SELECT jaysan_model_subtype.* FROM jaysan_model_subtype inner join jaysan_model_type on jaysan_model_subtype.mtid = jaysan_model_type.mtid WHERE jaysan_model_type.pid in (SELECT jaysan_product_model.model_id from jaysan_product_model WHERE jaysan_product_model.product_id = $product_id) ORDER BY `mtid`  DESC";
+//  $sql = "SELECT jaysan_model_subtype.* FROM jaysan_model_subtype inner join jaysan_model_type on jaysan_model_subtype.mtid = jaysan_model_type.mtid WHERE jaysan_model_type.pid in (SELECT jaysan_product_model.model_id from jaysan_product_model WHERE jaysan_product_model.product_id = $product_id) ORDER BY `mtid`  DESC";
+
+$sql = "SELECT jaysan_model_subtype.* FROM jaysan_model_subtype WHERE jaysan_model_subtype.mtid = $type_id ORDER BY `mtid`  DESC";
 
 $result = $conn->query($sql);
 
