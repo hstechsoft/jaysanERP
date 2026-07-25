0aec767b119f6ef97674ef6b63d8480421d90ed2
nklharish1 <nklharish1@gmail.com>
2026-07-25 09:35:22 +0530

    part id added

--- php/get_jaysan_subtype_link.php
+++ php/get_jaysan_subtype_link.php
@@ -31,7 +31,7 @@ return $data;
 
 
 // get msid from jaysan_product_view
-$sql_get_msid = "SELECT parts_tbl.part_name, jpv.product_id,jpv.model_id,jpv.mtid,jpv.product_name,jpv.model_name,jpv.type_name,JSON_ARRAYAGG(JSON_OBJECT('msid', jpv.msid, 'subtype_name', jpv.subtype_name,'link_id', jaysan_subtype_link.link_id)) as sub_type FROM jaysan_product_view jpv
+$sql_get_msid = "SELECT parts_tbl.part_id,parts_tbl.part_name, jpv.product_id,jpv.model_id,jpv.mtid,jpv.product_name,jpv.model_name,jpv.type_name,JSON_ARRAYAGG(JSON_OBJECT('msid', jpv.msid, 'subtype_name', jpv.subtype_name,'link_id', jaysan_subtype_link.link_id)) as sub_type FROM jaysan_product_view jpv
  INNER JOIN jaysan_subtype_link on jpv.msid = jaysan_subtype_link.msid
  inner join parts_tbl on jaysan_subtype_link.part_id = parts_tbl.part_id
   WHERE ".$product_query." AND ".$model_query." AND ".$type_query." GROUP BY parts_tbl.part_id";
