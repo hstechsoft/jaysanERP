a88b581c44334690b50f292cc46186e9c7391a0a
nklharish1 <nklharish1@gmail.com>
2026-07-27 11:00:17 +0530

    dc_demand added

--- php/get_dc_demand_report.php
+++ php/get_dc_demand_report.php
@@ -1,5 +1,35 @@
--- Active: 1766385460907@@srv1002.hstgr.io@3306@u333142350_jaysan
-with iv_deatils as(select iv.work_process_id,iv.work_orders,iv.pending_process_qty,iv.input_part_id, 
+<?php
+ include 'db_head.php';
+
+ $des_godown = test_input($_GET['des_godown']);
+ $source_godown = test_input($_GET['source_godown']);
+ $process_id = test_input($_GET['process_id']);
+ $process_query =1;
+$godown_query =1;
+ if($process_id > 0){
+  $process_query = "iv.work_process_id = $process_id";
+ }
+
+
+ if($des_godown > 0){
+  $godown_query = "iv.godown = $des_godown";
+ }
+
+
+
+ 
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+
+return $data;
+}
+
+
+  
+
+ $sql = "with iv_deatils as(select iv.work_process_id,iv.work_orders,iv.pending_process_qty,iv.input_part_id, 
 if(iv.input_part_id is null,concat('semi finished part (',pt_final.part_name,')'),pt.part_name) as input_part_name,
 iv.previous_process_id,iv.required_qty,iv.godown,iv.dep,iv.sec,iv.total_reserve_qty,iv.needed,iv.dc_qty,iv.transport_qty from input_part_demand_view iv 
 
@@ -9,27 +39,27 @@ iv.previous_process_id,iv.required_qty,iv.godown,iv.dep,iv.sec,iv.total_reserve_
  left join parts_tbl pt_final on pwt_final.output_part <=> pt_final.part_id),
  job_work_reserved as (
     select sv.part_id, sv.process_id, sum(sv.reserve_qty) as total_reserve_qty, JSON_ARRAYAGG(JSON_OBJECT(
-        'same_godown', if(sv.godown = '1087',true,false),
+        'same_godown', if(sv.godown = '$source_godown',true,false),
         'godown', sv.godown,
         'dep', sv.dep,
         'sec', sv.sec,
         'reserve_qty', sv.reserve_qty,
         'stock_reserve_id', sv.stock_reserve_id,
         'stock_id', sv.stock_id
-    )) as stock_reserve_details from stock_view sv WHERE sv.reserve_type ="job_work_order" and sv.part_id is null
+    )) as stock_reserve_details from stock_view sv WHERE sv.reserve_type =\"job_work_order\" and sv.part_id is null
     group by sv.process_id
 
     union all
 
     select sv.part_id, sv.process_id, sum(sv.reserve_qty) as total_reserve_qty, JSON_ARRAYAGG(JSON_OBJECT(
-        'same_godown', if(sv.godown = '1087',true,false),
+        'same_godown', if(sv.godown = '$source_godown',true,false),
         'godown', sv.godown,
         'dep', sv.dep,
         'sec', sv.sec,
         'reserve_qty', sv.reserve_qty,
         'stock_reserve_id', sv.stock_reserve_id,
         'stock_id', sv.stock_id
-    )) as stock_reserve_details from stock_view sv WHERE sv.reserve_type ="job_work_order" and sv.part_id is not null
+    )) as stock_reserve_details from stock_view sv WHERE sv.reserve_type =\"job_work_order\" and sv.part_id is not null
     group by sv.part_id
  )
  select iv.work_process_id,iv.work_orders,iv.pending_process_qty,iv.godown,iv.dep,iv.sec,
@@ -50,5 +80,25 @@ iv.previous_process_id,iv.required_qty,iv.godown,iv.dep,iv.sec,iv.total_reserve_
  jpv.input_parts
  from iv_deatils iv  
  left join jaysan_process_view jpv on iv.work_process_id <=> jpv.process_id
- left join job_work_reserved jwr on iv.input_part_id <=> jwr.part_id and iv.previous_process_id <=> jwr.process_id GROUP BY iv.work_process_id,iv.godown
+ left join job_work_reserved jwr on iv.input_part_id <=> jwr.part_id and iv.previous_process_id <=> jwr.process_id where $process_query and $godown_query GROUP BY iv.work_process_id,iv.godown
+
+";
+ 
+// echo "sql: " . $sql . "<br>";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $rows = array();
+    while($r = mysqli_fetch_assoc($result)) {
+        $rows[] = $r;
+    }
+    print json_encode($rows);
+} else {
+  echo "0 result";
+}
+$conn->close();
+
+ ?>
+
 
