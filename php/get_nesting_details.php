083471f5a4aef20509babe1e6b5978c67ffc7be5
nklharish1 <nklharish1@gmail.com>
2026-08-18 17:47:36 +0530

    nesting details added

--- php/get_nesting_details.php
+++ php/get_nesting_details.php
@@ -43,7 +43,7 @@ return $data;
             'job_card_id', laser_job_card.job_card_id,
             'scarp_qty', laser_job_card.scarp_qty
         )
-    ) as laser_assigned_details, nes_work.material_qty, sum(ifnull(laser_job_card.qty, 0)) as total_assigned_qty, sum(ifnull(laser_job_card.qty, 0)) - ifnull(nes_work.material_qty, 0) as remaining_qty, nes_work.nesting_id
+    ) as laser_assigned_details, ifnull(nes_work.material_qty, 0) as material_qty, sum(ifnull(laser_job_card.qty, 0)) as total_assigned_qty, sum(ifnull(laser_job_card.qty, 0)) - ifnull(nes_work.material_qty, 0) as remaining_qty, nes_work.nesting_id
 from
     laser_job_card
     inner join nesting_details nes_work on laser_job_card.nesting_details_id = nes_work.nesting_details_id
