83662ebcb8261a008484f8f0f0afe0eed80d9319
nklharish1 <nklharish1@gmail.com>
2026-08-18 13:06:09 +0530

    laser master added

--- php/delete_nesting_master.php
+++ php/delete_nesting_master.php
@@ -15,6 +15,40 @@ $data = "'".$data."'";
 return $data;
 }
 
+// insert parts
+// get path of attachment
+$sql_get_path = "SELECT path FROM nesting_master WHERE nes_master_id = $nes_master_id";
+$result = $conn->query($sql_get_path);
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $path = $row["path"];
+  }
+} else {
+  echo "Error: " . $sql_get_path . "<br>" . $conn->error;
+  $conn->close();
+  exit;
+} 
+
+   $target_path = "../attachment/laser/nesting/".$path;
+  //  remove file if exists
+  if (file_exists($target_path)) {
+    unlink($target_path);
+  }
+    // delete parts
+
+
+// delete parts
+
+$sql_delete_parts = "DELETE FROM nesting_parts WHERE nesting_id = $nes_master_id";
+if ($conn->query($sql_delete_parts) === TRUE) {
+ 
+} else {
+  echo "Error: " . $sql_delete_parts . "<br>" . $conn->error;
+  $conn->close();
+  exit;
+}
+
 
  $sql =  "DELETE  FROM nesting_master WHERE nes_master_id =  $nes_master_id";
 
---
+++ php/get_nesting_master.php
@@ -0,0 +1,64 @@
+<?php
+ include 'db_head.php';
+
+ $nes_master_id = isset($_GET['nes_master_id']) ? $_GET['nes_master_id'] : 0;
+
+$master_query = 1;
+
+
+if($nes_master_id > 0){
+    $master_query = "nesting_master.nes_master_id = $nes_master_id";
+}
+
+
+ 
+ 
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+
+return $data;
+}
+
+
+ $sql = "with nesting_parts as(select nesting_id,JSON_ARRAYAGG(JSON_OBJECT(
+   'nes_part_id', nesting_parts.nes_part_id,
+    'part_id', nesting_parts.part_id,
+    'qty', nesting_parts.qty,
+    'part_name', parts_tbl.part_name
+)) as nesting_parts from nesting_parts
+inner join parts_tbl on nesting_parts.part_id = parts_tbl.part_id GROUP BY nesting_parts.nesting_id
+)
+select nesting_parts.*,nes_part.part_name as nesting_material ,nesting_master.created_by,
+nesting_master.material_id,
+nesting_master.nesting_name,
+nesting_master.path,
+nesting_master.run_time,
+nesting_master.nesting_type,
+nesting_master.std_length,
+employee.emp_name as created_by_name
+
+ from nesting_parts
+left join nesting_master on nesting_parts.nesting_id = nesting_master.nes_master_id
+left join parts_tbl nes_part on nes_part.part_id = nesting_master.material_id
+LEFT join employee on nesting_master.created_by = employee.emp_id 
+where $master_query
+";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $rows = array();
+    while($r = mysqli_fetch_assoc($result)) {
+        $rows[] = $r;
+    }
+    print json_encode($rows);
+} else {
+  echo "0 result";
+}
+$conn->close();
+
+ ?>
+
+
