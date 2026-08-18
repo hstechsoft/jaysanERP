8a665e24882ceaa35390c8742b14405341f614b4
nklharish1 <nklharish1@gmail.com>
2026-08-18 17:02:42 +0530

    nesting details added

--- php/get_nesting_details.php
+++ php/get_nesting_details.php
@@ -10,15 +10,15 @@ $material_id = test_input($_GET['material_id']);
 $material_id_query = 1;
 
 if($created_by != ''){
-    $created_by_query = "created_by = $created_by";
+    $created_by_query = "mas.created_by = $created_by";
 }
 
 if($nesting_name != ''){
-    $nesting_name_query = "nesting_name like '%$nesting_name%'";
+    $nesting_name_query = "mas.nesting_name like '%$nesting_name%'";
 }
 
 if($material_id != ''){
-    $material_id_query = "material_id = $material_id";
+    $material_id_query = "mas.material_id = $material_id";
 }
  
  
@@ -31,13 +31,39 @@ return $data;
 }
 
 
- $sql = "SELECT JSON_ARRAYAGG(
-        JSON_OBJECT('nes_part_id',nesting_parts.nes_part_id,'part_id', nesting_parts.part_id, 'qty', nesting_parts.qty, 'produced_qty', nesting_parts.produced_qty, 'scrap_qty', nesting_parts.scrap_qty, 'part_name', nesting_part.part_name)) as nesting_parts_details, created_by,path,nesting_name,nesting_details.nesting_id,material_id,material_qty,run_time,product,employee.emp_name,parts_tbl.part_name as material_name FROM nesting_details
- left join nesting_parts on nesting_details.nesting_id = nesting_parts.nesting_id
- left join parts_tbl nesting_part on nesting_parts.part_id = nesting_part.part_id
- inner join employee on nesting_details.created_by = employee.emp_id
-inner join parts_tbl on nesting_details.material_id = parts_tbl.part_id
-  WHERE $created_by_query and $nesting_name_query and $material_id_query  group by nesting_details.nesting_id";
+ $sql = "select
+    nd.created_by,
+    nd.material_qty,
+    nd.nesting_details_id,
+    nd.nesting_id,
+    emp.emp_name,
+    nest_part.part_name as part_name,
+    mat_part.part_name as material_name,
+    nd.run_time,
+    mas.nesting_name,
+    mas.material_id,
+    mas.path,
+    mas.std_length,
+    mas.run_time,
+    JSON_ARRAYAGG(
+        JSON_OBJECT(
+            'nes_part_id',
+            nesting_parts.nes_part_id,
+            'part_id',
+            nesting_parts.part_id,
+            'qty',
+            nesting_parts.qty,
+            'part_name',
+            nest_part.part_name
+        )
+    ) as nesting_parts_details  from
+    nesting_details nd
+    left join nesting_master mas on nd.nesting_id = mas.nes_master_id
+    left join nesting_parts on mas.nes_master_id = nesting_parts.nesting_id
+    left join parts_tbl nest_part on nesting_parts.part_id = nest_part.part_id
+    left join parts_tbl mat_part on mas.material_id = mat_part.part_id
+    left join employee emp on nd.created_by = emp.emp_id
+  WHERE $created_by_query and $nesting_name_query and $material_id_query  group by nd.nesting_id";
 
 
 $result = $conn->query($sql);
--- php/insert_nesting_details.php
+++ php/insert_nesting_details.php
@@ -1,15 +1,18 @@
-<?php
+ <?php
  include 'db_head.php';
 
+
+$material_qty = test_input($_POST['material_qty']);
  $created_by = test_input($_POST['created_by']);
 
 $nesting_name = test_input($_POST['nesting_name']);
 $material_id = test_input($_POST['material_id']);
-$material_qty = test_input($_POST['material_qty']);
+$nesting_type = test_input($_POST['nesting_type']);
+$std_length = test_input($_POST['std_length']);
 $run_time = test_input($_POST['run_time']);
-$product = test_input($_POST['product']);
 
-$nesting_parts = json_decode($_POST['nesting_parts'], true);
+
+$laser_parts = json_decode($_POST['laser_parts'], true);
 
 function test_input($data) {
 $data = trim($data);
@@ -25,58 +28,134 @@ try {
 // accept only pdf files
 
 
-$target_path = null; // initialize target path variable
+// get nesting id from nesting_master table if exists else insert new record in nesting_master table and get id
+$nesting_id = 0;
+$sql_check_nesting = "SELECT nesting_id FROM nesting_master WHERE nesting_name = $nesting_name";
+ $result_check_nesting = $conn->query($sql_check_nesting);
+if ($result_check_nesting->num_rows > 0) {
+    $row = $result_check_nesting->fetch_assoc();
+    $nesting_id = $row['nesting_id'];
+} 
  
 
+// if no nesting id found then insert new record in nesting_master table and get id
+if($nesting_id == 0) {
 
 
- $sql = "INSERT INTO nesting_details ( created_by,path,nesting_name,material_id,material_qty,run_time,product) VALUES ($created_by,'',$nesting_name,$material_id,$material_qty,$run_time,$product)";
+
+ $sql_insert_master = "INSERT INTO nesting_master ( created_by,path,nesting_name,material_id,nesting_type,std_length,run_time) VALUES ($created_by,'',$nesting_name,$material_id,$nesting_type,$std_length,$run_time)";
+
+  if ($conn->query($sql_insert_master) === TRUE) {
+// get inserted id 
+$nesting_id  = $conn->insert_id;
+  } else {
+    throw new Exception("Error: " . $sql_insert_master . "<br>" . $conn->error);  
+  }
+// insert parts
+foreach ($laser_parts as $parts) {
+      $part_id = $parts['part_id'];
+      $qty = $parts['qty'];
+
+      $sql_insert_parts = "INSERT INTO nesting_parts (nesting_id, part_id, qty) VALUES ($nesting_id, $part_id, $qty)";
+     
+      if ($conn->query($sql_insert_parts) === TRUE) {
+      } else {
+        throw new Exception("Error: " . $sql_insert_parts . "<br>" . $conn->error);
+      }
+
+}
+
+  // insert attachment  
+
+  // allow only pdf
+
+
+  if($_FILES['file']['name'] != ''){
+  
+//   echo $_FILES['file']['demo'];
+        $FileType = strtolower(pathinfo($_FILES['file']['name'],PATHINFO_EXTENSION));
+        if($FileType != "pdf" && $FileType != "jpg" && $FileType != "jpeg" && $FileType != "png" && $FileType != "gif" && $FileType != "bmp" && $FileType != "mp4" && $FileType != "mov" && $FileType != "wmv" && $FileType != "avi" && $FileType != "mkv" && $FileType != "webm") {
+            echo "Sorry, only PDF, JPG, JPEG, PNG, GIF, BMP, MP4, MOV, WMV, AVI, MKV & WEBM files are allowed.";
+            $conn->close();
+            exit;
+        }
+
+  
+    $target_path = "../attachment/laser/nesting/";
+   
+    
+
+     
+    if (!file_exists($target_path)) {
+        mkdir($target_path, 0755, true);
+    }
+$target_path = $target_path . "laser_" . $nesting_id . "." . $FileType;
+
+
+
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
+          
+            // update path in db
+            $sql_update_master = "UPDATE nesting_master SET path = 'attachment/laser/nesting/laser_" . $nesting_id . "." . $FileType . "' WHERE nes_master_id = $nesting_id";
+  
+  if ($conn->query($sql_update_master) === TRUE) {
+   
+  } else {
+   throw new Exception("Error: " . $sql_update_master . "<br>" . $conn->error);
+  }
+        } else{
+            echo "There was an error uploading the file, please try again!";
+        }
+
+        
+}
+}
+
+
+ $sql = "INSERT INTO nesting_details ( created_by,material_qty,nesting_id) VALUES ($created_by,$material_qty,$nesting_id)";
 
   if ($conn->query($sql) === TRUE) {
   // get inserted id
   $last_id = $conn->insert_id;
-  foreach($nesting_parts as $part) {
-    $part_id = $part['part_id'];
-    $quantity = $part['quantity'];
-    $sql_part = "INSERT INTO nesting_parts (nesting_id, part_id, qty) VALUES ($last_id, $part_id, $quantity) on duplicate key update qty = qty + $quantity";
-    if ($conn->query($sql_part) !== TRUE) {
-        throw new Exception("Error inserting nesting part: " . $conn->error);
-    }
-  }
+
 } else {
   throw new Exception("Error: " . $sql . "<br>" . $conn->error);
   }
+
+
 // remove '' from $nesting_name for file name
-$nesting_name = str_replace("'", "", $nesting_name);
+// $nesting_name = str_replace("'", "", $nesting_name);
 
-$file_name = $nesting_name . "_" . $last_id . ".pdf";
+// $file_name = $nesting_name . "_" . $last_id . ".pdf";
 
-// store directly in folder (not folder inside folder)
-$target_dir = __DIR__ . "/../nesting/";
+// // store directly in folder (not folder inside folder)
+// $target_dir = __DIR__ . "/../nesting/";
 
-if (!is_dir($target_dir)) {
-    mkdir($target_dir, 0777, true);
-}
+// if (!is_dir($target_dir)) {
+//     mkdir($target_dir, 0777, true);
+// }
 
-$target_path = $target_dir . $file_name;
+// $target_path = $target_dir . $file_name;
 
-if (move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
-    $response['upload-status'] = 'success';
-    $response['file_name'] = $file_name;
+// if (move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
+//     $response['upload-status'] = 'success';
+//     $response['file_name'] = $file_name;
   
-} else {
- throw new Exception("Error uploading file.");
-}
-$save_path = "nesting/" . $file_name;
+// } else {
+//  throw new Exception("Error uploading file.");
+// }
+// $save_path = "nesting/" . $file_name;
 
-// update path in database
-$update_sql = "UPDATE nesting_details SET path ='$save_path' WHERE nesting_id=$last_id";
-if ($conn->query($update_sql) === TRUE) {
-$response['status'] = 'success';
+// // update path in database
+// $update_sql = "UPDATE nesting_details SET path ='$save_path' WHERE nesting_id=$last_id";
+// if ($conn->query($update_sql) === TRUE) {
+// $response['status'] = 'success';
     
-} else {
-    throw new Exception("Error updating record: " . $conn->error);
-}
+// } else {
+//     throw new Exception("Error updating record: " . $conn->error);
+// }
+
+$response['status'] = 'success';
 
 $conn->commit();
 echo json_encode($response);
