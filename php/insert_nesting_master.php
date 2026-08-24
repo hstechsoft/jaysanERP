182f02de34b3d4c33f6725133630202db2fefa04
nklharish1 <nklharish1@gmail.com>
2026-08-24 13:35:11 +0530

    corrected

--- php/insert_nesting_master.php
+++ php/insert_nesting_master.php
@@ -38,7 +38,7 @@ $nes_master_id  = $conn->insert_id;
 foreach ($laser_parts as $parts) {
       $part_id = $parts['part_id'];
       $qty = $parts['qty'];
-      $weight = $parts['weight'];
+      $out_weight = $parts['weight'];
  
 
       $sql_insert_parts = "INSERT INTO nesting_parts (nesting_id, part_id, qty, weight) VALUES ($nes_master_id, $part_id, $qty, $weight)";
@@ -50,7 +50,53 @@ foreach ($laser_parts as $parts) {
 
 
       // create process_wel_tbl 
-      // get process_id from process_wel_tbl where process_title = 'laser cutting' and 
+      // get process_id from process_wel_tbl where process_title = 'laser cutting' and output_part = $part_id
+      $process_id = 0;
+      $sql_get_process_id = "SELECT process_wel_tbl.process_id FROM process_wel_tbl 
+      inner join jaysan_process on process_wel_tbl.process = jaysan_process.process_id
+      WHERE process_title = 'laser cutting' AND output_part = $part_id AND process_name = 'laser cutting'";
+      $result_get_process_id = $conn->query($sql_get_process_id);
+      if ($result_get_process_id->num_rows > 0) {
+          $row = $result_get_process_id->fetch_assoc();
+          $process_id = $row['process_id'];
+      }
+
+      if($process_id == 0){
+        // get process_id from jaysan_process where process_title = 'laser cutting'
+        $sql_get_process_id = "SELECT process_id FROM jaysan_process WHERE process_title = 'laser cutting'";
+        $result_get_process_id = $conn->query($sql_get_process_id);
+        if ($result_get_process_id->num_rows > 0) {
+            $row = $result_get_process_id->fetch_assoc();
+            $process = $row['process_id'];
+        }
+
+        // insert new record in process_wel_tbl with process_id, output_part = $part_id, process_name = 'laser cutting'
+        $sql_insert_process_wel_tbl = "INSERT INTO process_wel_tbl (process, output_part, component_cat, process_title) VALUES ($process, $part_id, 'laser cutting', 'laser cutting')";
+        if ($conn->query($sql_insert_process_wel_tbl) === TRUE) {
+        $new_process_id = $conn->insert_id;
+        } else {
+          throw new Exception("Error: " . $sql_insert_process_wel_tbl . "<br>" . $conn->error);
+        }
+
+
+        
+    if($cat == 'out')
+        {
+            $new_process_id_display =$new_process_id;
+            include_once 'update_final_process_id.php';
+            update_final_id($conn, $new_process_id);
+
+        }
+
+
+
+        // insert input_wel_parts for new process id
+        $insert_input_wel_parts_sql = "insert into input_wel_parts (process_id, input_part_id, qty) values ($new_process_id, $material_id,$out_weight);";
+        if ($conn->query($insert_input_wel_parts_sql) !== TRUE) {
+            throw new Exception("Error: " . $insert_input_wel_parts_sql . "<br>" . $conn->error);
+        }
+   
+      }
 
 }
 
