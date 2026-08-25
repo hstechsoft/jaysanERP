b8bdd416c243fee07225bfd5894a834137f9acb0
nklharish1 <nklharish1@gmail.com>
2026-08-25 11:01:31 +0530

    mrf corrected

--- php/get_internal_godown_stock_tally.php
+++ php/get_internal_godown_stock_tally.php
@@ -18,10 +18,15 @@ return $data;
  $sql = "SELECT
     tally_stock.qty,
     internal_godown.godown_name,
-    internal_godown.internal_godown_id as godown_id
+    internal_godown.internal_godown_id as godown_id,
+mrf.corrective_action,
+mrf.preventive_action,
+mrf.root_cause
 FROM
     internal_godown_stock_tally tally_stock
-INNER JOIN internal_godown ON tally_stock.godown_id = internal_godown.internal_godown_id WHERE mrf_id =  $mrf_id";
+INNER JOIN internal_godown ON tally_stock.godown_id = internal_godown.internal_godown_id 
+inner join material_request_form mrf on tally_stock.mrf_id = mrf.mrf_id
+WHERE mrf_id =  $mrf_id";
 
 $result = $conn->query($sql);
 
