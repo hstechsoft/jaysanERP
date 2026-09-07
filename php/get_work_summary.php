aeaa7e0cdd24f9e624328807daade25c6bf00beb
nklharish1 <nklharish1@gmail.com>
2026-09-07 17:25:41 +0530

    corrected

--- php/get_work_summary.php
+++ php/get_work_summary.php
@@ -220,7 +220,9 @@ foreach ($stcok_zero_array as $zero_stock)
         $pre_process_id = $zero_stock['previous_process_id'];
     }
 
-    $sql_insert_stock_requset = "INSERT  INTO stock_request (part_id, previous_process_id, qty, godown, dep, sec, emp_id,updated_at) VALUES ( {$zero_stock['part_id']},  $pre_process_id,   {$zero_stock['required_qty']} - {$zero_stock['available_qty']} , $godown_id, $dep_id, $sec_id, $emp_id, NOW()) on duplicate key update updated_at = NOW(), qty = qty + (  {$zero_stock['required_qty']} - {$zero_stock['available_qty']})";
+    
+
+    $sql_insert_stock_requset = "INSERT  INTO stock_request (part_id, previous_process_id, qty, godown, dep, sec, emp_id,updated_at) VALUES ( sql_nullable({$zero_stock['part_id']}),  sql_nullable($pre_process_id),   {$zero_stock['required_qty']} - {$zero_stock['available_qty']} , $godown_id, $dep_id, $sec_id, $emp_id, NOW()) on duplicate key update updated_at = NOW(), qty = qty + (  {$zero_stock['required_qty']} - {$zero_stock['available_qty']})";
     if ($conn->query($sql_insert_stock_requset) !== TRUE) {
         $result_json['message'] = "Error inserting stock request: " . $conn->error;
         echo json_encode($result_json);
