0314bb538edb9b9bf009ce895f02ba7b1cb3df7e
nklharish1 <nklharish1@gmail.com>
2026-09-11 13:12:37 +0530

    corrected

--- php/get_work_summary.php
+++ php/get_work_summary.php
@@ -465,11 +465,11 @@ $sql_get_time = "  SELECT  time_diff(
         qr_work_entry.start_time,
         qr_work_entry.end_time,
         'minute'
-    ) - sum(work_time_per_unit*qty) > 0,  (time_diff(
+    ) - sum(work_time_per_unit*qty) > 0,  (ifnull(time_diff(
         qr_work_entry.start_time,
         qr_work_entry.end_time,
         'minute'
-    ) ) - sum(work_time_per_unit*qty), 0 ) as free_time ,(SELECT sum(time_diff(qr1.start_time, qr1.end_time, 'minute')) FROM qr_work_entry qr1 WHERE  work_done_id = $work_done_id and production_id >  0 and qr1.end_time is not null and qr1.start_time >= qr_work_entry.start_time and qr1.end_time <= qr_work_entry.end_time) as total_qr_time FROM qr_work_entry 
+    ), 0) - ifnull(sum(work_time_per_unit*qty), 0 ), 0) as free_time ,(SELECT sum(time_diff(qr1.start_time, qr1.end_time, 'minute')) FROM qr_work_entry qr1 WHERE  work_done_id = $work_done_id and production_id >  0 and qr1.end_time is not null and qr1.start_time >= qr_work_entry.start_time and qr1.end_time <= qr_work_entry.end_time) as total_qr_time FROM qr_work_entry 
      LEFT join work_process on qr_work_entry.qr_work_id = work_process.current_work_id
      WHERE qr_work_id = $current_work_id group by qr_work_entry.qr_work_id";
     $result_json['sql_get_time'] = $sql_get_time;
